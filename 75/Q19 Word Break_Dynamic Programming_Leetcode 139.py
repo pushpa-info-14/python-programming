b3941f0f3eeb9085eaa74486de81df0d2dd87185
pushpa.info.14 <pushpa.info.14@gmail.com>
2024-02-19 23:05:17 +0530

    changes

--- 75/Q19 Word Break_Dynamic Programming_Leetcode 139.py
+++ 75/Q19 Word Break_Dynamic Programming_Leetcode 139.py
@@ -40,7 +40,7 @@ def word_break(s, word_dict):
 
     for i in range(len(s) - 1, -1, -1):
         for w in word_dict:
-            if (i + len(w)) <= len(s) and s[i : i + len(w)] == w:
+            if (i + len(w)) <= len(s) and s[i: i + len(w)] == w:
                 dp[i] = dp[i + len(w)]
             if dp[i]:
                 break
