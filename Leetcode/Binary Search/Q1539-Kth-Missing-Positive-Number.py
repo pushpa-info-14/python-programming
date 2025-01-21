c6e2af8607038c2e497d2948394d7137fba59f9f
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-01-21 21:59:35 +0530

    leetcode 704 1539

---
+++ Leetcode/Binary Search/Q1539-Kth-Missing-Positive-Number.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        l, r = 0, len(arr)
+        while l < r:
+            mid = (l + r)//2
+            if arr[mid] - (mid + 1) < k:
+                l = mid + 1
+            else:
+                r = mid
+
+        return l + k
+
+
+s = Solution()
+print(s.findKthPositive([2, 3, 4, 7, 11], 5))
+print(s.findKthPositive([1, 2, 3, 4], 2))
+print(s.findKthPositive([32, 59, 77], 1))
