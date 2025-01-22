d44085037838d5e3ad91a247064c3d194cefd246
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-01-22 22:46:22 +0530

    leetcode 981

--- Leetcode/Binary Search/Q981-Time-Based-Key-Value-Store.py
+++ Leetcode/Binary Search/Q981-Time-Based-Key-Value-Store.py
@@ -6,7 +6,7 @@ class TimeMap:
     def set(self, key: str, value: str, timestamp: int) -> None:
         if key not in self.store:
             self.store[key] = []
-        self.store[key].append((timestamp, value))
+        self.store[key].append([timestamp, value])
 
     def get(self, key: str, timestamp: int) -> str:
         if key not in self.store:
