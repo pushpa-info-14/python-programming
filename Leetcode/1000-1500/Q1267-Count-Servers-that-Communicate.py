4408c0c147453cd45acc56bd8f98e209623c2818
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-01-23 08:27:53 +0530

    leetcode 1267

---
+++ Leetcode/1000-1500/Q1267-Count-Servers-that-Communicate.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def countServers(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        row = [0 for i in range(m)]
+        col = [0 for i in range(n)]
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]:
+                    col[j] += 1
+                    row[i] += 1
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] and (row[i] > 1 or col[j]>1):
+                    count += 1
+        return count
+
+s = Solution()
+print(s.countServers([[1,0],[0,1]]))
+print(s.countServers([[1,0],[1,1]]))
+print(s.countServers([[1,1,0,0],[0,0,1,0],[0,0,1,0],[0,0,0,1]]))
