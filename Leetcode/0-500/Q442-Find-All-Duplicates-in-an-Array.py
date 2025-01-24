c4259c75b9315b4d314aee30e09db57b9137dd6d
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-01-24 23:20:12 +0530

    leetcode 442

---
+++ Leetcode/0-500/Q442-Find-All-Duplicates-in-an-Array.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+
+        res = []
+        for num in nums:
+            num = abs(num)
+            if nums[num - 1] < 0:
+                res.append(num)
+            nums[num -1] = -nums[num - 1]
+
+        return res
+
+s = Solution()
+print(s.findDuplicates([4,3,2,7,8,2,3,1]))
+print(s.findDuplicates([1,1,2]))
+print(s.findDuplicates([1]))
