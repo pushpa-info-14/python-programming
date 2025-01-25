7419144920d8e0e100481769a6598f261cd756aa
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-01-25 23:04:31 +0530

    leetcode 2962

---
+++ Leetcode/2500-3000/Q2962-Count-Subarrays-Where-Max-Element-Appears-at-Least-K-Times.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        max_n, max_count = max(nums), 0
+        l = 0
+        res = 0
+        for r in range(len(nums)):
+            if nums[r] == max_n:
+                max_count += 1
+            # while max_count > k or (l <= r and max_count == k and nums[l] != max_n):
+            #     if nums[l] == max_n:
+            #         max_count -= 1
+            #     l += 1
+            # if max_count == k:
+            #     res += l + 1
+            while max_count == k:
+                if nums[l] == max_n:
+                    max_count -= 1
+                l += 1
+            res += l
+        return res
+
+s = Solution()
+print(s.countSubarrays([1,3,2,3,3],2))
+print(s.countSubarrays([1,4,2,1],3))
