9e54e2709e5bc70d96de97acf2ff1298dd64ca10
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-01-28 20:14:12 +0530

    leetcode 405

---
+++ Leetcode/0-500/Q405-Convert-a-Number-to-Hexadecimal.py
@@ -0,0 +1,23 @@
+from math import remainder
+
+
+class Solution:
+    def toHex(self, num: int) -> str:
+        if num == 0: return "0"
+
+        digits = ["0","1","2","3","4","5","6","7","8","9","a", "b", "c", "d", "e", "f"]
+        res = ""
+
+        if num < 0:
+            num = pow(2, 32) + num
+        while num:
+            rem = num%16
+            num = num // 16
+            res = digits[rem] + res
+
+        return res
+
+s = Solution()
+print(s.toHex(26))
+print(s.toHex(-1))
+print(s.toHex(-2))
