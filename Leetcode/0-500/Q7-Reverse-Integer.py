60e1c4a420b975c83b83b8d80b42031ce421396a
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-01-31 23:35:11 +0530

    leetcode 7

---
+++ Leetcode/0-500/Q7-Reverse-Integer.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        n = x
+        if x < 0:
+            n = abs(x)
+
+        res = 0
+        while n:
+            res = res * 10
+            res += n % 10
+            n = n // 10
+
+        if res > 2**31 - 1:
+            return 0
+        return res if x > 0 else -res
+
+s = Solution()
+print(s.reverse(123))
+print(s.reverse(-123))
