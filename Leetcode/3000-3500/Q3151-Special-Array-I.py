79e0ace4ca454b4eabd360a7bcf7bd58dd8ee72d
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-02-01 10:08:49 +0530

    leetcode 3151

---
+++ Leetcode/3000-3500/Q3151-Special-Array-I.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        for i in range(1, len(nums)):
+            if nums[i - 1] % 2 == nums[i] % 2:
+                return False
+        return True
+
+
+s = Solution()
+print(s.isArraySpecial([1]))
+print(s.isArraySpecial([2, 1, 4]))
+print(s.isArraySpecial([4, 3, 1, 6]))
+print(s.isArraySpecial([1, 5]))
