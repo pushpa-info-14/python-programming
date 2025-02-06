cf0ace0067818fe619f6c38df9608a72a905edca
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-02-06 20:45:16 +0530

    leetcode 367 2349 changes

--- Leetcode/0-500/Q367-Valid-Perfect-Square.py
+++ Leetcode/0-500/Q367-Valid-Perfect-Square.py
@@ -2,14 +2,14 @@ class Solution:
     def isPerfectSquare(self, num: int) -> bool:
 
         l, r = 0, num
-        while l < r:
-            mid = l + (r - l + 1) // 2
+        while l <= r:
+            mid = (l + r) // 2
             power = mid * mid
 
             if power == num:
                 return True
             elif power < num:
-                l = mid
+                l = mid + 1
             else:
                 r = mid - 1
         return False
--- Leetcode/2000-2500/Q2349-Design-a-Number-Container-System.py
+++ Leetcode/2000-2500/Q2349-Design-a-Number-Container-System.py
@@ -1,3 +1,4 @@
+import heapq
 from collections import defaultdict
 from sortedcontainers import SortedSet
 
@@ -6,26 +7,58 @@ class NumberContainers:
 
     def __init__(self):
         self.index_to_number = {}
-        self.number_to_indexes = defaultdict(SortedSet)
+        self.number_to_indices = defaultdict(SortedSet)
 
     def change(self, index: int, number: int) -> None:
         if index in self.index_to_number and self.index_to_number[index] != number:
             previous_number = self.index_to_number[index]
-            self.number_to_indexes[previous_number].remove(index)
-            if not self.number_to_indexes[previous_number]:
-                del self.number_to_indexes[previous_number]
+            self.number_to_indices[previous_number].remove(index)
+            if not self.number_to_indices[previous_number]:
+                del self.number_to_indices[previous_number]
         self.index_to_number[index] = number
-        self.number_to_indexes[number].add(index)
+        self.number_to_indices[number].add(index)
 
     def find(self, number: int) -> int:
-        if number in self.number_to_indexes and len(self.number_to_indexes[number]) > 0:
-            return self.number_to_indexes[number][0]
+        if number in self.number_to_indices and len(self.number_to_indices[number]) > 0:
+            return self.number_to_indices[number][0]
         else:
             return -1
+
+class NumberContainers2:
+
+    def __init__(self):
+        self.index_to_number = {}
+        self.number_to_indices = defaultdict(list)
+
+    def change(self, index: int, number: int) -> None:
+        self.index_to_number[index] = number
+        heapq.heappush(self.number_to_indices[number], index)
+
+    def find(self, number: int) -> int:
+        if number not in self.number_to_indices:
+            return -1
+
+        while self.number_to_indices[number]:
+            index = self.number_to_indices[number][0]
+
+            if self.index_to_number[index] == number:
+                return index
+            heapq.heappop(self.number_to_indices[number])
+        return -1
 
 
 # Your NumberContainers object will be instantiated and called as such:
 obj = NumberContainers()
+print(obj.find(10))
+print(obj.change(2, 10))
+print(obj.change(1, 10))
+print(obj.change(3, 10))
+print(obj.change(5, 10))
+print(obj.find(10))
+print(obj.change(1, 20))
+print(obj.find(10))
+
+obj = NumberContainers2()
 print(obj.find(10))
 print(obj.change(2, 10))
 print(obj.change(1, 10))
