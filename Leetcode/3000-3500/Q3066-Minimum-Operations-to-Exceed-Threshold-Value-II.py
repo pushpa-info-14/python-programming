fb341136c046ebb251bf9c39c60d25d100b0da0e
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-02-10 19:24:37 +0530

    leetcode 3066

---
+++ Leetcode/3000-3500/Q3066-Minimum-Operations-to-Exceed-Threshold-Value-II.py
@@ -0,0 +1,23 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+        operations = 0
+
+        while nums:
+            if nums[0] < k:
+                x = heapq.heappop(nums)
+                y = heapq.heappop(nums)
+                z = min(x, y) * 2 + max(x, y)
+                heapq.heappush(nums, z)
+                operations += 1
+            else:
+                return operations
+
+
+s = Solution()
+print(s.minOperations([2, 11, 10, 1, 3], 10))
+print(s.minOperations([1, 1, 2, 4, 9], 20))
