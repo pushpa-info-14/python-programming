6860044d3c52fdaf9a16c7d4566e9d6bb53b36aa
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-02-11 20:20:12 +0530

    leetcode 1749

---
+++ Leetcode/1500-2000/Q1749-Maximum-Absolute-Sum-of-Any-Subarray.py
@@ -0,0 +1,36 @@
+from itertools import accumulate
+from typing import List
+
+
+class Solution:
+    def maxAbsoluteSum(self, nums: List[int]) -> int:
+        max_sum, max_cur = 0, 0
+        min_sum, min_cur = 0, 0
+
+        for num in nums:
+            max_cur = max(max_cur + num, 0)
+            max_sum = max(max_sum, max_cur)
+
+            min_cur = min(min_cur + num, 0)
+            min_sum = min(min_sum, min_cur)
+        return max(max_sum, -min_sum)
+
+    def maxAbsoluteSum2(self, nums: List[int]) -> int:
+        s = list(accumulate(nums, initial=0))
+        return max(s) - min(s)
+
+    def maxAbsoluteSum3(self, nums: List[int]) -> int:
+        accumulated_sum = [0]
+
+        for num in nums:
+            accumulated_sum.append(num + accumulated_sum[-1])
+        return max(accumulated_sum) - min(accumulated_sum)
+
+
+s = Solution()
+print(s.maxAbsoluteSum([1, -3, 2, 3, -4]))
+print(s.maxAbsoluteSum([2, -5, 1, -4, 3, -2]))
+print(s.maxAbsoluteSum2([1, -3, 2, 3, -4]))
+print(s.maxAbsoluteSum2([2, -5, 1, -4, 3, -2]))
+print(s.maxAbsoluteSum3([1, -3, 2, 3, -4]))
+print(s.maxAbsoluteSum3([2, -5, 1, -4, 3, -2]))
