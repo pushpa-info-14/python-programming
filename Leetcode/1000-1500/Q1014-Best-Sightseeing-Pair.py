f06214a46aec974431a473b490dfffc5019c13f4
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-02-12 00:13:31 +0530

    leetcode 1014

---
+++ Leetcode/1000-1500/Q1014-Best-Sightseeing-Pair.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        res = 0
+
+        cur_max = values[0] - 1
+        for i in range(1, len(values)):
+            res = max(res, values[i] + cur_max)
+            cur_max = max(cur_max - 1, values[i] - 1)
+        return res
+
+
+s = Solution()
+print(s.maxScoreSightseeingPair([8, 1, 5, 2, 6]))
+print(s.maxScoreSightseeingPair([1, 2]))
