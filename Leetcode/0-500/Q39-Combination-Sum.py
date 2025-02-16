1a749a29077950d0eeca54c0357e2044881f5ff5
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-02-16 21:58:01 +0530

    leetcode 39

--- Leetcode/0-500/Q39-Combination-Sum.py
+++ Leetcode/0-500/Q39-Combination-Sum.py
@@ -6,6 +6,7 @@ class Solution:
         n = len(candidates)
         res = []
         cur = []
+
         def backtrack(i, target):
             if target == 0:
                 res.append(cur.copy())
@@ -22,8 +23,33 @@ class Solution:
         backtrack(0, target)
         return res
 
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        n = len(candidates)
+        res = []
+        cur = []
+
+        def backtrack(i, target):
+            if target == 0:
+                res.append(cur.copy())
+                return
+            elif i == n or target < 0:
+                return
+
+            candidate = candidates[i]
+            cur.append(candidate)
+            backtrack(i, target - candidate)
+            cur.pop()
+
+            backtrack(i + 1, target)
+
+        backtrack(0, target)
+        return res
+
 
 s = Solution()
 print(s.combinationSum([2, 3, 6, 7], 7))
 print(s.combinationSum([2, 3, 5], 8))
 print(s.combinationSum([2], 1))
+print(s.combinationSum2([2, 3, 6, 7], 7))
+print(s.combinationSum2([2, 3, 5], 8))
+print(s.combinationSum2([2], 1))
