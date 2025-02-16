04c681b0ac1e592d52a564077a779b04ecaad952
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-02-16 11:18:23 +0530

    leetcode 1718

--- Leetcode/1500-2000/Q1718-Construct-the-Lexicographically-Largest-Valid-Sequence.py
+++ Leetcode/1500-2000/Q1718-Construct-the-Lexicographically-Largest-Valid-Sequence.py
@@ -35,7 +35,50 @@ class Solution:
         backtracking(0)
         return result_sequence
 
+    def constructDistancedSequence2(self, n: int) -> List[int]:
+        size = n * 2 - 1
+        res = [0] * size
+        used = set()  # nums already placed
+
+        def backtracking(i):
+            if i == size:
+                return True
+            # Try largest elements
+            for num in reversed(range(1, n + 1)):
+                # Validation
+                if num in used:
+                    continue
+                if num > 1 and (i + num >= size or res[i + num]):
+                    continue
+
+                # Try decision
+                used.add(num)
+                res[i] = num
+                if num > 1:
+                    res[i + num] = num
+
+                j = i + 1
+                while j < size and res[j] > 0:
+                    j += 1
+
+                # Recursive step
+                if backtracking(j):
+                    return True
+
+                # Backtrack
+                used.remove(num)
+                res[i] = 0
+                if num > 1:
+                    res[i + num] = 0
+
+            return False
+
+        backtracking(0)
+        return res
+
 
 s = Solution()
 print(s.constructDistancedSequence(3))
 print(s.constructDistancedSequence(10))
+print(s.constructDistancedSequence2(3))
+print(s.constructDistancedSequence2(10))
