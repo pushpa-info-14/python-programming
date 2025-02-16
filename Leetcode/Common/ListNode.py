835ef6d54700d1517c42fb9c5c982479945ed7c0
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-02-16 15:30:41 +0530

    ListNode

---
+++ Leetcode/Common/ListNode.py
@@ -0,0 +1,12 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def print(self):
+        res = []
+        cur = self
+        while cur:
+            res.append(cur.val)
+            cur = cur.next
+        return res
