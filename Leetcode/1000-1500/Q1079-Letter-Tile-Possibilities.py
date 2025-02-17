a2b93fa1aff57f62191c80051d97f04e0e8f83ae
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-02-17 22:01:52 +0530

    leetcode 1079

--- Leetcode/1000-1500/Q1079-Letter-Tile-Possibilities.py
+++ Leetcode/1000-1500/Q1079-Letter-Tile-Possibilities.py
@@ -1,29 +1,22 @@
+from collections import defaultdict
+
+
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
-
-        res = [0]
-
-        def dfs(tile_map):
-            res[0] += 1
-            for c in list(tile_map.keys()):
-                tile_map[c] -= 1
-                if tile_map[c] == 0:
-                    del tile_map[c]
-
-                dfs(tile_map)
-
-                if c not in tile_map:
-                    tile_map[c] = 0
-                tile_map[c] += 1
-
-        frequency = {}
+        count = defaultdict(int)
         for c in tiles:
-            if c not in frequency:
-                frequency[c] = 0
-            frequency[c] += 1
+            count[c] += 1
 
-        dfs(frequency)
-        return res[0] - 1
+        def dfs():
+            res = 0
+            for c in count:
+                if count[c] > 0:
+                    count[c] -= 1
+                    res += 1
+                    res += dfs()
+                    count[c] += 1
+            return res
+        return dfs()
 
 
 s = Solution()
