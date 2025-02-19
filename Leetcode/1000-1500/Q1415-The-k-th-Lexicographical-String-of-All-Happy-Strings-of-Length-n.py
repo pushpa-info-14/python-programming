a4ac05f3e0ce90b0ec7e7ae5c93298678fed91c1
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-02-19 18:46:50 +0530

    leetcode 1415

--- Leetcode/1000-1500/Q1415-The-k-th-Lexicographical-String-of-All-Happy-Strings-of-Length-n.py
+++ Leetcode/1000-1500/Q1415-The-k-th-Lexicographical-String-of-All-Happy-Strings-of-Length-n.py
@@ -20,8 +20,35 @@ class Solution:
         backtracking("c")
         return res[0] if happy_count[0] == k else ""
 
+    def getHappyString2(self, n: int, k: int) -> str:
+        total_happy = 3 * 2 ** (n - 1)
+
+        res = []
+        choices = "abc"
+        left, right = 1, total_happy
+
+        for i in range(n):
+            cur = left
+            partition_size = (right - left + 1) // len(choices)
+            # Polling: 1 - 4, 5 - 8, 9 - 12
+
+            for c in choices:
+                # cur <= k < cur + partition_size
+                if k in range(cur, cur + partition_size):
+                    res.append(c)
+                    left = cur
+                    right = cur + partition_size - 1
+                    choices = "abc".replace(c, "")
+                    break
+                cur += partition_size
+        return "".join(res)
+
 
 s = Solution()
 print(s.getHappyString(1, 3))
 print(s.getHappyString(1, 4))
 print(s.getHappyString(3, 9))
+
+print(s.getHappyString2(1, 3))
+print(s.getHappyString2(1, 4))
+print(s.getHappyString2(3, 9))
