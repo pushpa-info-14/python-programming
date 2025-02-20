4b7e08109f563ac794bc07b72640b98c77731b7e
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-02-20 15:41:40 +0530

    leetcode 1980

---
+++ Leetcode/1500-2000/Q1980 Find Unique Binary String.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        n = len(nums)
+        nums_set = set(nums)
+        res = [""]
+
+        def backtrack(cur):
+            if len(cur) == n:
+                if cur in nums_set:
+                    return False
+                res[0] = cur
+                return True
+            if backtrack(cur + "1") or backtrack(cur + "0"):
+                return True
+            return False
+
+        backtrack("")
+        return res[0]
+
+
+s = Solution()
+print(s.findDifferentBinaryString(["01", "10"]))
+print(s.findDifferentBinaryString(["00", "01"]))
+print(s.findDifferentBinaryString(["111", "011", "001"]))
