8759211c9b6d87edf96bb7358c7e5308c5967be8
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-02-23 20:51:22 +0530

    leetcode 1092

---
+++ Leetcode/1000-1500/Q1092 Shortest Common Supersequence.py
@@ -0,0 +1,8 @@
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        return ""
+
+
+s = Solution()
+print(s.shortestCommonSupersequence("abac", "cab"))
+print(s.shortestCommonSupersequence("aaaaaaaa", "aaaaaaaa"))
