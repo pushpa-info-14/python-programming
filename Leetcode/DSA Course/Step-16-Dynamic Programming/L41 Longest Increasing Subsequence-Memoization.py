c25f555084bfd7b318ffe9262bf6cb32c17c1a1b
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-03-04 20:39:25 +0530

    longest increasing subsequence-memoization

---
+++ Leetcode/DSA Course/Step-16-Dynamic Programming/L41 Longest Increasing Subsequence-Memoization.py
@@ -0,0 +1,37 @@
+def longestIncreasingSubsequence(nums):
+    n = len(nums)
+
+    def dfs(index, prev_index):
+        if index == n:
+            return 0
+        length = 0 + dfs(index + 1, prev_index)  # Not take
+        if prev_index == -1 or nums[index] > nums[prev_index]:
+            length = max(length, 1 + dfs(index + 1, index))
+        return length
+
+    return dfs(0, -1)
+
+
+def longestIncreasingSubsequenceMemo(nums):
+    n = len(nums)
+    dp = [[-1] * (n + 1) for _ in range(n)]
+
+    def dfs(index, prev_index):
+        if index == n:
+            return 0
+        if dp[index][prev_index + 1] != -1:
+            return dp[index][prev_index + 1]
+        length = 0 + dfs(index + 1, prev_index)  # Not take
+        if prev_index == -1 or nums[index] > nums[prev_index]:
+            length = max(length, 1 + dfs(index + 1, index))
+
+        dp[index][prev_index + 1] = length
+        return length
+
+    return dfs(0, -1)
+
+
+# Take it
+# Not take it
+print(longestIncreasingSubsequence([10, 9, 2, 5, 3, 7, 101, 18]))
+print(longestIncreasingSubsequenceMemo([10, 9, 2, 5, 3, 7, 101, 18]))
