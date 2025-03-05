5999c488b2ea39a2952716e296811f2bc5a14e1d
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-03-05 19:02:09 +0530

    leetcode 1043

--- Leetcode/DSA Course/Step-16-Dynamic Programming/L55 Q1043 Partition Array for Maximum Sum.py
+++ Leetcode/DSA Course/Step-16-Dynamic Programming/L55 Q1043 Partition Array for Maximum Sum.py
@@ -25,7 +25,29 @@ class Solution:
         return dfs(0)
 
 
+    def maxSumAfterPartitioningTabulation(self, arr: List[int], k: int) -> int:
+        n = len(arr)
+        dp = [0] * (n + 1)
+        dp[n] = 0
+
+        for index in range(n-1, -1, -1):
+            length = 0
+            max_val = -math.inf
+            res = -math.inf
+            for i in range(index, min(index + k, n)):
+                length += 1
+                max_val = max(max_val, arr[i])
+                cur_sum = length * max_val + dp[i + 1]
+                res = max(res, cur_sum)
+            dp[index] = res
+
+        return dp[0]
+
+
 s = Solution()
 print(s.maxSumAfterPartitioning([1, 15, 7, 9, 2, 5, 10], 3))
 print(s.maxSumAfterPartitioning([1, 4, 1, 5, 7, 3, 6, 1, 9, 9, 3], 4))
 print(s.maxSumAfterPartitioning([1], 1))
+print(s.maxSumAfterPartitioningTabulation([1, 15, 7, 9, 2, 5, 10], 3))
+print(s.maxSumAfterPartitioningTabulation([1, 4, 1, 5, 7, 3, 6, 1, 9, 9, 3], 4))
+print(s.maxSumAfterPartitioningTabulation([1], 1))
