3831513a2a6bf1102aad79c331e3d1f7091a87dd
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-03-07 21:47:00 +0530

    combination sum II

--- Leetcode/DSA Course/Step-7-Recursion/L9 Q40 Combination Sum II.py
+++ Leetcode/DSA Course/Step-7-Recursion/L9 Q40 Combination Sum II.py
@@ -24,8 +24,35 @@ class Solution:
         dfs(0, [], 0)
         return res
 
+    def combinationSum22(self, candidates: List[int], target: int) -> List[List[int]]:
+        n = len(candidates)
+        candidates.sort()
+        res = []
+
+        def dfs(i, cur, summation):
+            if summation > target:
+                return
+            if summation == target:
+                res.append(cur.copy())
+                return
+
+            for j in range(i, n):
+                if j > i and candidates[j] == candidates[j - 1]: continue
+                if candidates[j] > target: break
+
+                cur.append(candidates[j])
+                dfs(j + 1, cur, summation + candidates[j])
+                cur.pop()
+
+        dfs(0, [], 0)
+        return res
+
 
 s = Solution()
-print(s.combinationSum([10, 1, 2, 7, 6, 1, 5], 8))
-print(s.combinationSum([2, 5, 2, 1, 2], 5))
-print(s.combinationSum([2], 1))
+print(s.combinationSum2([10, 1, 2, 7, 6, 1, 5], 8))
+print(s.combinationSum2([2, 5, 2, 1, 2], 5))
+print(s.combinationSum2([2], 1))
+
+print(s.combinationSum22([10, 1, 2, 7, 6, 1, 5], 8))
+print(s.combinationSum22([2, 5, 2, 1, 2], 5))
+print(s.combinationSum22([2], 1))
