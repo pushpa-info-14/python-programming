25f0be45317c79397693d9ddcf9baf21efd34456
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-03-09 09:45:26 +0530

    leetcode 36

---
+++ Leetcode/0-500/Q36 Valid Sudoku.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squares = defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                d = board[r][c]
+                if d in rows[r] or d in cols[c] or d in squares[(r // 3, c // 3)]:
+                    return False
+                rows[r].add(d)
+                cols[c].add(d)
+                squares[(r // 3, c // 3)].add(d)
+        return True
+
+
+s = Solution()
+print(s.isValidSudoku([["5", "3", ".", ".", "7", ".", ".", ".", "."],
+                       ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+                       [".", "9", "8", ".", ".", ".", ".", "6", "."],
+                       ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+                       ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+                       ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+                       [".", "6", ".", ".", ".", ".", "2", "8", "."],
+                       [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+                       [".", ".", ".", ".", "8", ".", ".", "7", "9"]]))
