4ba91a4c8841d8c4505f2733b09aae640fd847f2
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-03-11 19:13:25 +0530

    leetcode 1358

--- Leetcode/1000-1500/Q1358 Number of Substrings Containing All Three Characters.py
+++ Leetcode/1000-1500/Q1358 Number of Substrings Containing All Three Characters.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
         n = len(s)
@@ -13,9 +16,30 @@ class Solution:
             r += 1
         return res
 
+    def numberOfSubstrings2(self, s: str) -> int:
+        n = len(s)
+        chars = defaultdict(int)
+        l = 0
+        r = 0
+        res = 0
+        while r < n:
+            chars[s[r]] += 1
+            while len(chars) == 3:
+                res += n - r
+                chars[s[l]] -= 1
+                if chars[s[l]] == 0:
+                    del chars[s[l]]
+                l += 1
+            r += 1
+        return res
+
 
 s = Solution()
 print(s.numberOfSubstrings("abcabc"))
 print(s.numberOfSubstrings("aaacb"))
 print(s.numberOfSubstrings("abc"))
 print(s.numberOfSubstrings("acbbcac"))
+print(s.numberOfSubstrings2("abcabc"))
+print(s.numberOfSubstrings2("aaacb"))
+print(s.numberOfSubstrings2("abc"))
+print(s.numberOfSubstrings2("acbbcac"))
