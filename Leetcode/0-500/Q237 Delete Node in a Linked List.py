d59beaa5be1f03f6a69f4286a336bf79d0a725c7
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-03-12 21:51:44 +0530

    leetcode 237

---
+++ Leetcode/0-500/Q237 Delete Node in a Linked List.py
@@ -0,0 +1,13 @@
+from Leetcode.Common.ListNode import ListNode
+
+
+class Solution:
+    def deleteNode(self, node: ListNode):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+
+
