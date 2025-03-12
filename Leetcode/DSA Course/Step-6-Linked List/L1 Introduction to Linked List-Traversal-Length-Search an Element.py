6631522860d73e09fb61f6cf4245500fea9d88e3
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-03-12 20:41:18 +0530

    intro to linked list

---
+++ Leetcode/DSA Course/Step-6-Linked List/L1 Introduction to Linked List-Traversal-Length-Search an Element.py
@@ -0,0 +1,45 @@
+from Leetcode.Common.ListNode import ListNode
+
+
+def createLinkedList(nums):
+    n = len(nums)
+    temp = ListNode(0)
+    cur = temp
+    for i in range(n):
+        cur.next = ListNode(nums[i])
+        cur = cur.next
+    return temp.next
+
+
+def printLengthOfLinkedList(head: ListNode):
+    count = 0
+    cur = head
+    while cur is not None:
+        count += 1
+        cur = cur.next
+    print(count)
+
+
+def printLinkedList(head: ListNode):
+    res = []
+    cur = head
+    while cur is not None:
+        res.append(cur.val)
+        cur = cur.next
+    print(res)
+
+
+def search(head: ListNode, target):
+    cur = head
+    while cur is not None:
+        if cur.val == target:
+            return True
+        cur = cur.next
+    return False
+
+
+ls = createLinkedList([1, 2, 3, 4, 5])
+printLengthOfLinkedList(ls)
+printLinkedList(ls)
+print(search(ls, 5))
+print(search(ls, 7))
