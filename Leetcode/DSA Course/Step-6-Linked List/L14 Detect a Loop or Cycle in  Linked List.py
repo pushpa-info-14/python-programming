d58e3a68ca87cf4232590ed57616ca5a6daf22f5
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-03-21 21:44:47 +0530

    detect a loop or cycle in a linked list

---
+++ Leetcode/DSA Course/Step-6-Linked List/L14 Detect a Loop or Cycle in  Linked List.py
@@ -0,0 +1,33 @@
+from Leetcode.Common.ListNode import ListNode
+
+
+def check(head: ListNode):
+    mp = {}
+    cur = head
+    while cur:
+        if cur in mp:
+            return True
+        else:
+            mp[cur] = 1
+    return False
+
+
+def check2(head: ListNode):
+    slow = head
+    fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False
+
+
+l = ListNode.create([1, 2, 3, 4, 5, 6, 7, 8, 9])
+last = l
+while last.next:
+    last = last.next
+last.next = l.next.next.next
+
+print(check(l))
+print(check2(l))
