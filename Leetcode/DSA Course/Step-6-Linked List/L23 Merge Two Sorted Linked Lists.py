09e4a35b67c25e8ddd94e692959b8cbcfd840e5a
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-03-22 19:30:29 +0530

    merge two sorted linked list

---
+++ Leetcode/DSA Course/Step-6-Linked List/L23 Merge Two Sorted Linked Lists.py
@@ -0,0 +1,35 @@
+from Leetcode.Common.ListNode import ListNode
+
+
+def merge(head1: ListNode, head2: ListNode):
+    dummy = ListNode(-1)
+
+    temp = dummy
+    temp1 = head1
+    temp2 = head2
+    while temp1 and temp2:
+        if temp1.val < temp2.val:
+            temp.next = temp1
+            temp1 = temp1.next
+        else:
+            temp.next = temp2
+            temp2 = temp2.next
+        temp = temp.next
+
+    while temp1:
+        temp.next = temp1
+        temp1 = temp1.next
+        temp = temp.next
+
+    while temp2:
+        temp.next = temp2
+        temp2 = temp2.next
+        temp = temp.next
+
+    return dummy.next
+
+
+l1 = ListNode.create([2, 4, 8, 10])
+l2 = ListNode.create([1, 3, 3, 6, 11, 14])
+l = merge(l1, l2)
+l.print()
