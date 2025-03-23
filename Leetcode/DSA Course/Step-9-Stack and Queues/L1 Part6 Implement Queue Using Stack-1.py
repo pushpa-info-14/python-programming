67a20377ed65b9a16b3cb268e4c6150a2ab0b8df
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-03-23 16:37:31 +0530

    queue using stack approach 1

---
+++ Leetcode/DSA Course/Step-9-Stack and Queues/L1 Part6 Implement Queue Using Stack-1.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+
+class Queue:
+    def __init__(self):
+        self.s1 = deque()
+        self.s2 = deque()
+
+    def push(self, x):
+        while len(self.s1):
+            self.s2.append(self.s1[-1])
+            self.s1.pop()
+        self.s1.append(x)
+        while len(self.s2):
+            self.s1.append(self.s2[-1])
+            self.s2.pop()
+
+    def pop(self):
+        return self.s1.pop()
+
+    def peek(self):
+        return self.s1[-1]
+
+    def size(self):
+        return len(self.s1)
+
+
+queue = Queue()
+queue.push(1)
+queue.push(2)
+print(queue.peek())
+print(queue.pop())
+print(queue.peek())
+print(queue.pop())
+print(queue.size())
