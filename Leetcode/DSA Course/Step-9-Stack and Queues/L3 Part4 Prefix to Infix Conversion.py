4784241680226ac8beb046e22410536ccb15194f
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-03-23 19:38:48 +0530

    prefix to infix

---
+++ Leetcode/DSA Course/Step-9-Stack and Queues/L3 Part4 Prefix to Infix Conversion.py
@@ -0,0 +1,27 @@
+priority = {
+    "^": 4,
+    "*": 3,
+    "/": 3,
+    "+": 2,
+    "-": 2,
+    "(": 0,
+}
+
+
+def prefix_to_infix(s: str) -> str:
+    n = len(s)
+    stack = []
+    i = n - 1
+    while i >= 0:
+        if s[i].isalnum():
+            stack.append(s[i])
+        else:
+            t1 = stack.pop()
+            t2 = stack.pop()
+            combined = "(" + t1 + s[i] + t2 + ")"
+            stack.append(combined)
+        i -= 1
+    return stack[0]
+
+
+print(prefix_to_infix("*+pq-mn"))
