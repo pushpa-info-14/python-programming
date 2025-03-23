d8df07057d1578aeacc847902f403ec9806bc649
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-03-23 11:40:14 +0530

    stack using array

---
+++ Leetcode/DSA Course/Step-9-Stack and Queues/L1 Part1 Implement Stack Using Array.py
@@ -0,0 +1,33 @@
+class Stack:
+    def __init__(self, n):
+        self.array = [0] * n
+        self.top = -1
+
+    def push(self, x):
+        self.top += 1
+        self.array[self.top] = x
+
+    def peek(self):
+        if self.top == -1:
+            print("empty")
+            return None
+        return self.array[self.top]
+
+    def pop(self):
+        if self.top == -1:
+            print("empty")
+            return None
+        self.top -= 1
+        return self.array[self.top + 1]
+
+    def size(self):
+        return self.top + 1
+
+
+stack = Stack(10)
+stack.push(1)
+stack.push(2)
+print(stack.peek())
+print(stack.pop())
+print(stack.peek())
+print(stack.pop())
