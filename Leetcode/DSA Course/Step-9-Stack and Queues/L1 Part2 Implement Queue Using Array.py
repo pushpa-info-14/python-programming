cae71c710709f80da331d5d623e799273890f06b
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-03-23 15:39:07 +0530

    queue using array

---
+++ Leetcode/DSA Course/Step-9-Stack and Queues/L1 Part2 Implement Queue Using Array.py
@@ -0,0 +1,50 @@
+class Queue:
+    def __init__(self, n):
+        self.size = n
+        self.array = [0] * n
+        self.cur_size = 0
+        self.start = -1
+        self.end = -1
+
+    def push(self, x):
+        if self.cur_size == self.size:
+            print("Queue is full")
+            return None
+        if self.cur_size == 0:
+            self.start = 0
+            self.end = 0
+        else:
+            self.end = (self.end + 1) % self.size
+        self.array[self.end] = x
+        self.cur_size += 1
+
+    def pop(self):
+        if self.cur_size == 0:
+            print("Queue is empty")
+            return None
+        element = self.array[self.start]
+        if self.cur_size == 1:
+            self.start = -1
+            self.end = -1
+        else:
+            self.start = (self.start + 1) % self.size
+        self.cur_size -= 1
+        return element
+
+    def peek(self):
+        if self.cur_size == 0:
+            print("Queue is empty")
+            return None
+        return self.array[self.start]
+
+    def size(self):
+        return self.cur_size
+
+
+queue = Queue(10)
+queue.push(1)
+queue.push(2)
+print(queue.peek())
+print(queue.pop())
+print(queue.peek())
+print(queue.pop())
