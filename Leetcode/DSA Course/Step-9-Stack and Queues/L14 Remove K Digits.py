d2f338b82c1f7c62d5db1b3a7b47ecaa7349bfd6
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-03-27 22:49:24 +0530

    remove k digits

--- Leetcode/DSA Course/Step-9-Stack and Queues/L14 Remove K Digits.py
+++ Leetcode/DSA Course/Step-9-Stack and Queues/L14 Remove K Digits.py
@@ -1,28 +1,29 @@
-def removeDigits(s: str, k: int):
-    stack = []
-    for c in s:
-        while stack and k > 0 and ord(stack[-1]) > ord(c):
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        stack = []
+        for c in num:
+            while stack and k > 0 and ord(stack[-1]) > ord(c):
+                stack.pop()
+                k -= 1
+            stack.append(c)
+
+        while k:
             stack.pop()
             k -= 1
-        stack.append(c)
+        if not stack:
+            return "0"
 
-    while k:
-        stack.pop()
-        k -= 1
-    if not stack:
-        return "0"
+        res = []
+        while stack:
+            res.append(stack.pop())
 
-    res = []
-    while stack:
-        res.append(stack.pop())
+        while res and res[-1] == "0":
+            res.pop()
 
-    while res and res[-1] == "0":
-        res.pop()
+        if not res:
+            return "0"
 
-    if not res:
-        return "0"
-
-    return "".join(reversed(res))
+        return "".join(reversed(res))
 
 
 # Remove maximum k digits from left to right
@@ -31,6 +32,7 @@ Edge cases: k == n "0"
 000100 --> 1000
 123456 k=3 123
 """
-print(removeDigits("1432219", 3))
-print(removeDigits("100002", 3))
-print(removeDigits("100002", 1))
+s = Solution()
+print(s.removeKdigits("1432219", 3))
+print(s.removeKdigits("100002", 3))
+print(s.removeKdigits("100002", 1))
