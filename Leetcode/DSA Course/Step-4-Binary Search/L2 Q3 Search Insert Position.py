f7bc0387d2059a8288af15ac6eedee669739fa86
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-03-31 22:39:52 +0530

    search insert position

---
+++ Leetcode/DSA Course/Step-4-Binary Search/L2 Q3 Search Insert Position.py
@@ -0,0 +1,19 @@
+def search(nums, target):
+    n = len(nums)
+    low = 0
+    high = n - 1
+    res = n
+    while low <= high:
+        mid = (low + high) // 2
+        if nums[mid] >= target:
+            res = mid
+            high = mid - 1
+        else:
+            low = mid + 1
+    return res
+
+
+print(search([1, 2, 3, 3, 5, 8, 8, 10, 10, 11], 6))
+print(search([1, 2, 3, 3, 5, 8, 8, 10, 10, 11], 12))
+print(search([1, 2, 3, 3, 5, 8, 8, 10, 10, 11], 4))
+print(search([1, 2, 3, 3, 5, 8, 8, 10, 10, 11], 3))
