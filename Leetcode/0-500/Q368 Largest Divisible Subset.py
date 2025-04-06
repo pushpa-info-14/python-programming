c657933155a0b5ab611ce01083a4c0dd37a5f5d8
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-04-06 23:46:16 +0530

    leetcode 368

--- Leetcode/0-500/Q368 Largest Divisible Subset.py
+++ Leetcode/0-500/Q368 Largest Divisible Subset.py
@@ -64,44 +64,13 @@ class Solution:
                 res = dp[i]
         return res
 
-    def largestDivisibleSubset4(self, nums: List[int]) -> List[int]:
-        nums.sort()
-        n = len(nums)
-        dp = [1] * n
-        mp = [0] * n
-        cur_max = 0
-        last_index = 0
-
-        for i in range(n):
-            mp[i] = i
-            for prev in range(i):
-                if nums[i] % nums[prev] == 0 and 1 + dp[prev] > dp[i]:
-                    dp[i] = 1 + dp[prev]
-                    mp[i] = prev
-            if dp[i] > cur_max:
-                cur_max = dp[i]
-                last_index = i
-
-        temp = [nums[last_index]]
-        while mp[last_index] != last_index:
-            last_index = mp[last_index]
-            temp.append(nums[last_index])
-        return temp[::-1]
-
 
 s = Solution()
-print(s.largestDivisibleSubset([1, 16, 7, 8, 4]))
 print(s.largestDivisibleSubset([1, 2, 3]))
 print(s.largestDivisibleSubset([1, 2, 4, 8]))
 
-print(s.largestDivisibleSubset2([1, 16, 7, 8, 4]))
 print(s.largestDivisibleSubset2([1, 2, 3]))
 print(s.largestDivisibleSubset2([1, 2, 4, 8]))
 
-print(s.largestDivisibleSubset3([1, 16, 7, 8, 4]))
 print(s.largestDivisibleSubset3([1, 2, 3]))
 print(s.largestDivisibleSubset3([1, 2, 4, 8]))
-
-print(s.largestDivisibleSubset4([1, 16, 7, 8, 4]))
-print(s.largestDivisibleSubset4([1, 2, 3]))
-print(s.largestDivisibleSubset4([1, 2, 4, 8]))
