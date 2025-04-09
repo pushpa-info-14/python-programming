4df8ba0709fdf0036fee53692a0978170bf69acd
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-04-09 20:57:56 +0530

    implement trie-insert, search and starts with

---
+++ Leetcode/DSA Course/Step-17-Tries/L1 Implement Trie-1-Insert-Search-StartsWith.py
@@ -0,0 +1,48 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.flag = False
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c in cur.children:
+                cur = cur.children[c]
+            else:
+                cur.children[c] = TrieNode()
+                cur = cur.children[c]
+        cur.flag = True
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                return False
+            else:
+                cur = cur.children[c]
+        return cur.flag
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            else:
+                cur = cur.children[c]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+trie = Trie()
+trie.insert("apple")
+print(trie.search("apple"))
+print(trie.search("app"))
+print(trie.startsWith("app"))
+trie.insert("app")
+print(trie.search("app"))
