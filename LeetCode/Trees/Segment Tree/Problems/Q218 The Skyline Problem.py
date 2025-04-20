3c108ab34c2c1c8a7a17d041459394585a35acef
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-04-20 23:21:04 +0530

    leetcode 218

--- LeetCode/Trees/Segment Tree/Problems/Q218 The Skyline Problem.py
+++ LeetCode/Trees/Segment Tree/Problems/Q218 The Skyline Problem.py
@@ -1,32 +1,61 @@
 from typing import List
 
+from typing import List
 
 class SegmentTree:
-    def __init__(self, n):
-        self.n = n
-        self.tree = [0] * (4 * n)
+    def __init__(self, size):
+        self.size = size
+        self.tree = [0] * (4 * size + 1)
+        self.isLazy = [False] * (4 * size + 1)
+        self.pendingUpdates = [0] * (4 * size + 1)
 
-    def update(self, v, tl, tr, l, r, val):
-        if tl > r or tr < l:
-            return
-        if l <= tl and tr <= r:
-            self.tree[v] = max(self.tree[v], val)
-            return
-        mid = (tl + tr) // 2
-        self.update(2 * v, tl, mid, l, r, val)
-        self.update(2 * v + 1, mid + 1, tr, l, r, val)
-
-    def query(self, v, tl, tr, index):
-        if tl > index or tr < index:
+    def _query(self, v, tl, tr, l, r):
+        if tr < l or tl > r:  # No overlap
             return 0
-        if tl == tr:
+        if l <= tl and tr <= r:  # Full overlap
             return self.tree[v]
-        mid = (tl + tr) // 2
 
-        if index <= mid:
-            return max(self.tree[v], self.query(2 * v, tl, mid, index))
-        else:
-            return max(self.tree[v], self.query(2 * v + 1, mid + 1, tr, index))
+        # Partial overlap
+        self._pushDown(v, tl, tr)
+        tm = (tl + tr) // 2
+        left_ans = self._query(2 * v, tl, tm, l, r)
+        right_ans = self._query(2 * v + 1, tm + 1, tr, l, r)
+        return max(left_ans , right_ans)
+
+    def _update(self, v, tl, tr, l, r, val):
+        if r < tl or tr < l:  # No overlap
+            return
+        if l <= tl and tr <= r:  # Full overlap
+            self._apply(v, tl, tr, val)
+            return
+
+        # Partial overlap
+        self._pushDown(v, tl, tr)
+        tm = (tl + tr) // 2
+        self._update(2 * v, tl, tm, l, r, val)
+        self._update(2 * v + 1, tm + 1, tr, l, r, val)
+        self.tree[v] = max(self.tree[2 * v] , self.tree[2 * v + 1])
+
+    def _pushDown(self, v, tl, tr):
+        if not self.isLazy[v]:
+            return
+        self.isLazy[v] = False
+        tm = (tl + tr) // 2
+        self._apply(2 * v, tl, tm, self.pendingUpdates[v])
+        self._apply(2 * v + 1, tm + 1, tr, self.pendingUpdates[v])
+        self.pendingUpdates[v] = 0
+
+    def _apply(self, v, tl, tr, val):
+        if tl != tr:  # Leaf nodes can't be lazy
+            self.isLazy[v] = True
+            self.pendingUpdates[v] = max(self.pendingUpdates[v], val)
+        self.tree[v] = max(self.tree[v],  val)
+
+    def query(self, l, r):
+        return self._query(1, 0, self.size - 1, l, r)
+
+    def update(self, l, r, val):
+        self._update(1, 0, self.size - 1, l, r, val)
 
 
 class Solution:
@@ -36,11 +65,11 @@ class Solution:
 
         tree = SegmentTree(len(points))
         for L, R, H in buildings:
-            tree.update(1, 0, len(points) - 1, index[L], index[R] - 1, H)
+            tree.update(index[L], index[R] - 1, H)
 
         result, prev_height = [], 0
         for x in points:
-            height = tree.query(1, 0, len(points) - 1, index[x])
+            height = tree.query(index[x], index[x])
             if height != prev_height:
                 result.append([x, height])
                 prev_height = height
