86eae7dec194587ed7446e66bcb5389a78bfc877
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-04-27 21:20:54 +0530

    leetcode 547

---
+++ LeetCode/DSA Course/Step-15-Graphs/L7 Q547 Number Of Provinces.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+
+        visited = set()
+        res = 0
+
+        def dfs(node):
+            visited.add(node)
+            for nei in range(n):
+                if isConnected[node][nei] and nei not in visited:
+                    dfs(nei)
+
+        for i in range(n):
+            if i not in visited:
+                dfs(i)
+                res += 1
+        return res
+
+
+s = Solution()
+print(s.findCircleNum([[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
+print(s.findCircleNum([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
