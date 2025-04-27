ed24a03277a4f9a5cb9299f155f3669e837ceae0
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-04-27 19:21:37 +0530

    intro to graph

---
+++ LeetCode/DSA Course/Step-15-Graphs/L1 Introduction to Graph - Types - Different Convensions Used.py
@@ -0,0 +1,20 @@
+"""
+Directed Graph
+Undirected Graph
+Node/Vertex
+Edge
+Directed Edge
+Undirected Edge
+
+Cyclic Graph
+Acyclic Graph
+
+Path - Contains a lot of nodes and each of them are reachable
+
+Degree of graph
+Property: Total degree of a graph is 2 x E
+In-degree - Number of incoming edges
+Out-degree - Number of outgoing edges
+
+Edge weights
+"""
