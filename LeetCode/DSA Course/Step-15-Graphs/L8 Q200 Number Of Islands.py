1aeae60b1b0f3df10c44fd6f409dcca6286cdfae
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-04-27 22:12:07 +0530

    leetcode 200

---
+++ LeetCode/DSA Course/Step-15-Graphs/L8 Q200 Number Of Islands.py
@@ -0,0 +1,43 @@
+from typing import List
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+
+        visited = [[False] * n for _ in range(m)]
+        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+
+        def dfs(r, c):
+            visited[r][c] = True
+            for dr, dc in directions:
+                nr = r + dr
+                nc = c + dc
+
+                if nr < 0 or nc < 0 or nr == m or nc == n or visited[nr][nc] or grid[nr][nc] == '0':
+                    continue
+                dfs(nr, nc)
+
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                if not visited[i][j] and grid[i][j] == '1':
+                    dfs(i, j)
+                    res += 1
+        return res
+
+
+s = Solution()
+print(s.numIslands(grid=[
+    ["1", "1", "1", "1", "0"],
+    ["1", "1", "0", "1", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "0", "0", "0"]
+]))
+print(s.numIslands(grid=[
+    ["1", "1", "0", "0", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "1", "0", "0"],
+    ["0", "0", "0", "1", "1"]
+]))
