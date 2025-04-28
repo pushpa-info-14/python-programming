92c48ddc22d7eea44b4977b32d4b5f01b071a3e6
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-04-28 23:24:44 +0530

    leetcode 694

--- LeetCode/DSA Course/Step-15-Graphs/L16 Q694 Number of Distinct Islands.py
+++ LeetCode/DSA Course/Step-15-Graphs/L16 Q694 Number of Distinct Islands.py
@@ -7,35 +7,25 @@ class Solution:
         m = len(arr[0])
         visited = [[0] * m for _ in range(n)]
         directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-        island_map = {}
 
-        def dfs(r, c, index):
+        def dfs(r, c, coordinates, r0, c0):
             visited[r][c] = 1
-            island_map[count].append((r, c))
+            coordinates.append((r - r0, c - c0))
             for dr, dc in directions:
                 nr = r + dr
                 nc = c + dc
                 if nr < 0 or nr == n or nc < 0 or nc == m or visited[nr][nc] or arr[nr][nc] == 0:
                     continue
-                dfs(nr, nc, index)
+                dfs(nr, nc, coordinates, r0, c0)
 
-        count = 0
+        unique = set()
         for i in range(n):
             for j in range(m):
                 if not visited[i][j] and arr[i][j] == 1:
-                    island_map[count] = []
-                    dfs(i, j, count)
-                    count += 1
-
-        unique = set()
-        for key in island_map.keys():
-            island = island_map[key]
-            island.sort()
-            min_r = island[0][0]
-            min_c = island[0][1]
-            for i in range(len(island)):
-                island[i] = (island[i][0] - min_r, island[i][1] - min_c)
-            unique.add(tuple(island))
+                    co = []
+                    dfs(i, j, co, i, j)
+                    co.sort()
+                    unique.add(tuple(co))
 
         return len(unique)
 
