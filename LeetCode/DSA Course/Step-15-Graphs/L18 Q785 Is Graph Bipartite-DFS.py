fd7b851dcaad17a38481c5ffb4502fa1cff6be5b
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-04-29 20:01:57 +0530

    leetcode 785

---
+++ LeetCode/DSA Course/Step-15-Graphs/L18 Q785 Is Graph Bipartite-DFS.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        n = len(graph)
+        color = [-1] * n
+
+        def dfs(node, col):
+            color[node] = col
+            for nei in graph[node]:
+                if color[nei] == -1:
+                    if not dfs(nei, not color[node]):
+                        return False
+                elif color[nei] == color[node]:
+                    return False
+            return True
+
+        for i in range(n):
+            if color[i] == -1:
+                if not dfs(i, 0):
+                    return False
+        return True
+
+
+"""
+- Color the graph with 2 colors such that no adjacent nodes have same color.
+- Linear graph with no cycles are always bipartite.
+- Any graph with even cycle length are always bipartite.
+"""
+s = Solution()
+print(s.isBipartite(graph=[[1, 2, 3], [0, 2], [0, 1, 3], [0, 2]]))
+print(s.isBipartite(graph=[[1, 3], [0, 2], [1, 3], [0, 2]]))
