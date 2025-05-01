850e6ed1309098490af43079aeeae9c0a8d174c0
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-05-01 17:43:44 +0530

    cleanups

---
+++ Data Structures and Algorithms/DSA Course/Step-15-Graphs/L29 Q127 Word Ladder-I-Shortest Paths.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+
+
+        return 0
+
+s=Solution()
+print(s.ladderLength(beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]))
+print(s.ladderLength(beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]))
---
+++ Data Structures and Algorithms/Trees/Segment Tree/Problems/Q3072 Distribute Elements Into Two Arrays II.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def resultArray(self, nums: List[int]) -> List[int]:
+
+        return []
+
+s=Solution()
+print(s.resultArray([2,1,3,3]))
+print(s.resultArray([5,14,3,1,2]))
+print(s.resultArray([3,3,3,3]))
