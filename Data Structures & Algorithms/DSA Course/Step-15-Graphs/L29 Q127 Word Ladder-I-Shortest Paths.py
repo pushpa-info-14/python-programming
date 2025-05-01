1913bbc6ca6f0144c835b6f81f9652179ad2e6fe
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-05-01 19:42:08 +0530

    word ladder I

--- Data Structures & Algorithms/DSA Course/Step-15-Graphs/L29 Q127 Word Ladder-I-Shortest Paths.py
+++ Data Structures & Algorithms/DSA Course/Step-15-Graphs/L29 Q127 Word Ladder-I-Shortest Paths.py
@@ -1,12 +1,29 @@
+from collections import deque
 from typing import List
 
 
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        q = deque()
+        q.append((beginWord, 1))
+        words = set(wordList)
 
+        while q:
+            word, l = q.popleft()
+            for i in range(len(word)):
+                chars = list(word)
+                for j in range(26):
+                    chars[i] = chr(j + ord('a'))
+                    new_word = "".join(chars)
+                    if new_word in words:
+                        if new_word == endWord:
+                            return l + 1
+                        q.append((new_word, l + 1))
+                        words.remove(new_word)
 
         return 0
 
-s=Solution()
-print(s.ladderLength(beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]))
-print(s.ladderLength(beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]))
+
+s = Solution()
+print(s.ladderLength(beginWord="hit", endWord="cog", wordList=["hot", "dot", "dog", "lot", "log", "cog"]))
+print(s.ladderLength(beginWord="hit", endWord="cog", wordList=["hot", "dot", "dog", "lot", "log"]))
