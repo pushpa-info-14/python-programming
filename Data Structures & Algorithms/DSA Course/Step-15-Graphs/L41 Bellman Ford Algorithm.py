851122eb6dbc5919e1d58dbcd05ba713f636b666
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-05-03 14:53:52 +0530

    bellman ford

---
+++ Data Structures & Algorithms/DSA Course/Step-15-Graphs/L41 Bellman Ford Algorithm.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+def shortestPath(n: int, edges: List[List[int]], src: int) -> List[int]:
+    int_max = 10 ** 8
+    distance = [int_max] * n
+    distance[src] = 0
+    for i in range(n - 1):
+        count = 0
+        for u, v, w in edges:
+            if distance[u] != int_max and distance[v] > distance[u] + w: # u must be reachable
+                distance[v] = distance[u] + w
+                count += 1
+        if count == 0:
+            return distance
+    for u, v, w in edges:
+        if distance[u] != int_max and distance[v] > distance[u] + w: # u must be reachable
+            return [-1]
+
+    return distance
+
+
+"""
+Dijkstra's algorithm cannot handle negative edges and cycles.
+
+Bellman Ford works on a directed graph.
+Relax all the edges n-1 times sequentially.
+"""
+
+print(shortestPath(6, [[3, 2, 6], [5, 3, 1], [0, 1, 5], [1, 5, -3], [1, 2, -2], [3, 4, -2], [2, 4, 3]], 0))
+print(shortestPath(8, [[1, 0, -4], [3, 5, -4], [4, 3, -5], [5, 3, -10]], 1))
