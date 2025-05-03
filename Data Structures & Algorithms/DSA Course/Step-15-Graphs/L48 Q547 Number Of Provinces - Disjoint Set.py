d2abfe950d9b8211a40f14d0d774620d4de5e00b
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-05-03 22:07:01 +0530

    leetcode 547

---
+++ Data Structures & Algorithms/DSA Course/Step-15-Graphs/L48 Q547 Number Of Provinces - Disjoint Set.py
@@ -0,0 +1,44 @@
+from typing import List
+
+
+class DisjointSet:
+    def __init__(self, n):
+        self.size = [0] * (n + 1)
+        self.parent = list(range(n + 1))
+
+    def findParent(self, x):
+        if x != self.parent[x]:
+            self.parent[x] = self.findParent(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        x = self.findParent(x)
+        y = self.findParent(y)
+        if x != y:
+            if self.size[x] < self.size[y]:
+                self.parent[x] = y
+                self.size[y] += self.size[x]
+            else:
+                self.parent[y] = x
+                self.size[x] += self.size[y]
+
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        ds = DisjointSet(n)
+
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j]:
+                    ds.union(i, j)
+        res = 0
+        for i in range(n):
+            if ds.findParent(i) == i:
+                res += 1
+        return res
+
+
+s = Solution()
+print(s.findCircleNum([[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
+print(s.findCircleNum([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
