d451d06a7a818dcc06bfdd3785e21bc5e4aed17a
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-05-03 18:00:16 +0530

    mst

---
+++ Data Structures & Algorithms/DSA Course/Step-15-Graphs/L44 Minimum Spanning Tree-Theory.py
@@ -0,0 +1,10 @@
+"""
+The Minimum Spanning Tree (MST) is the collection of edges required
+to connect all vertices in an undirected graph, with the minimum total
+edge weight. The animation above runs Prim's algorithm to find the MST.
+Another way to find the MST, which also works for unconnected graphs,
+is to run Kruskal's algorithm
+
+A tree in which we have N nodes and N -1 edges and all the nodes are
+reachable from each other
+"""
