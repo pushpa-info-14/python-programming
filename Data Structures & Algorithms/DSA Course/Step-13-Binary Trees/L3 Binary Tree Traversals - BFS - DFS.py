98604be622fb0b8181cd45160daa24d1fb5ba256
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-05-04 20:18:06 +0530

    traversals

---
+++ Data Structures & Algorithms/DSA Course/Step-13-Binary Trees/L3 Binary Tree Traversals - BFS - DFS.py
@@ -0,0 +1,8 @@
+"""
+DFS:
+    Inorder Traversal (Left + Root + Right)
+    Preorder Traversal (Root + Left + Right)
+    Postorder Traversal (Left + Right + Root)
+BFS:
+    Level Order traversal
+"""
