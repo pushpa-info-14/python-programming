dcd42f989396df9b33349042a277b7ecf40db711
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-05-04 20:10:56 +0530

    intro to trees

---
+++ Data Structures & Algorithms/DSA Course/Step-13-Binary Trees/L1 Introduction to Trees - Type of Trees.py
@@ -0,0 +1,14 @@
+"""
+Types:
+    1. Full Binary Tree
+        - Every node either has 0 or 2 children
+    2. Complete Binary Tree
+        - All the level are completely filled except the last level
+        - The last level has all the nodes in left as possible
+    3. Perfect Binary Tree
+        - All leaf nodes are at the same level
+    4. Balanced Binary Tree
+        - Height of tree at max log(N) (N = number of nodes)
+    5. Degenerate Tree (Skewed Tree/ Pathological Tree)
+        - Each parent node has only one child node
+"""
