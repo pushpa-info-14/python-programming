e1c28e459d51a5fb66866ea21d219cb079463ab9
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-05-16 21:54:23 +0530

    leetcode 2900

--- LeetCode/2500-3000/Q2900 Longest Unequal Adjacent Groups Subsequence I.py
+++ LeetCode/2500-3000/Q2900 Longest Unequal Adjacent Groups Subsequence I.py
@@ -12,7 +12,26 @@ class Solution:
 
         return [words[i] for i in valid_indexes]
 
+    def getLongestSubsequence2(self, words: List[str], groups: List[int]) -> List[str]:
+        n = len(words)
+        dp = [[words[i]] for i in range(n)]
+
+        for i in range(n):
+            for j in range(i):
+                if groups[i] != groups[j]:
+                    if len(dp[i]) < len(dp[j]) + 1:
+                        copy = dp[j].copy()
+                        copy.append(words[i])
+                        dp[i] = copy
+        res = []
+        for items in dp:
+            if len(res) < len(items):
+                res = items
+        return res
+
 
 s = Solution()
 print(s.getLongestSubsequence(words=["e", "a", "b"], groups=[0, 0, 1]))
 print(s.getLongestSubsequence(words=["a", "b", "c", "d"], groups=[1, 0, 1, 1]))
+print(s.getLongestSubsequence2(words=["e", "a", "b"], groups=[0, 0, 1]))
+print(s.getLongestSubsequence2(words=["a", "b", "c", "d"], groups=[1, 0, 1, 1]))
