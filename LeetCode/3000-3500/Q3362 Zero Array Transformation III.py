5b5d787864208ec34d10e10af64861234bf01584
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-05-22 21:26:21 +0530

    leetcode 3362

--- LeetCode/3000-3500/Q3362 Zero Array Transformation III.py
+++ LeetCode/3000-3500/Q3362 Zero Array Transformation III.py
@@ -12,19 +12,25 @@ class Solution:
         ans = 0
         j = 0
         for i in range(n):
+            # Step1: Push all queries starting at ith index to candidate pool
             while j < q and queries[j][0] == i:
                 heapq.heappush(candidates, -queries[j][1])
                 j += 1
 
+            # Step2: Reuse if any query was already included and valid
             nums[i] -= len(chosen)
 
+            # Step3: If more queries are required then get them from candidate pool and push them to chosen pool
             while nums[i] > 0 and len(candidates) > 0 and -candidates[0] >= i:
                 ans += 1
                 heapq.heappush(chosen, -heapq.heappop(candidates))
                 nums[i] -= 1
 
+            # Step4: If insufficient queries, return -1
             if nums[i] > 0:
                 return -1
+
+            # Step5: Remove queries ending at ith index from chosen pool
             while len(chosen) > 0 and chosen[0] == i:
                 heapq.heappop(chosen)
 
