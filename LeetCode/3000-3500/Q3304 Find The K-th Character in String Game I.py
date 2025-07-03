532e0399f805805b5aa6ee43256c50f088d6265d
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-07-03 21:57:01 +0530

    leetcode 3304

---
+++ LeetCode/3000-3500/Q3304 Find The K-th Character in String Game I.py
@@ -0,0 +1,24 @@
+class Solution:
+    def kthCharacter(self, k: int) -> str:
+        word = 'a'
+        while len(word) < k:
+            w = ''
+            for c in word:
+                w += chr(ord('a') + (ord(c) + 1 - ord('a')) % 26)
+            word += w
+        return word[k - 1]
+
+    def kthCharacter2(self, k: int) -> str:
+        word = [0]
+        while len(word) < k:
+            w = [i + 1 for i in word]
+            word = word + w
+        return chr(word[k - 1] % 26 + ord('a'))
+
+
+
+s = Solution()
+print(s.kthCharacter(5))
+print(s.kthCharacter(10))
+print(s.kthCharacter2(5))
+print(s.kthCharacter2(10))
