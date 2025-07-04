f4a93c788f057675d97903c5f6e358599c40cb7e
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-07-04 17:26:05 +0530

    leetcode 3307

---
+++ LeetCode/Important/Q3307 Find The K-th Character in String Game II.py
@@ -0,0 +1,42 @@
+from math import log
+from typing import List
+
+
+class Solution:
+    def kthCharacter(self, k: int, operations: List[int]) -> str:
+        val = k - 1
+        steps = 0
+
+        while val > 0:
+            p = int(log(val, 2))
+            lower = 1 << p
+            if operations[p]:
+                steps += 1
+            val -= lower
+        return chr(97 + (steps % 26))
+
+    def kthCharacter2(self, k: int, operations: List[int]) -> str:
+        def solve(n):
+            if n == 1:
+                return 0
+            if n.bit_count() == 1:
+                return operations[n.bit_length() - 2] + solve(n // 2)
+
+            msb_v = 1 << (n.bit_length() - 1)
+            pred = n - msb_v
+
+            op_index = n.bit_length() - 1
+            return operations[op_index] + solve(pred)
+
+        shifts = solve(k)
+
+        shifts %= 26
+
+        return chr(ord("a") + shifts)
+
+
+s = Solution()
+print(s.kthCharacter(k=5, operations=[0, 0, 0]))
+print(s.kthCharacter(k=10, operations=[0, 1, 0, 1]))
+print(s.kthCharacter2(k=5, operations=[0, 0, 0]))
+print(s.kthCharacter2(k=10, operations=[0, 1, 0, 1]))
