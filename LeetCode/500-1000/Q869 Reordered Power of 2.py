765eeae11fdeba27b386167c29c5a37e99df1f30
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-08-10 20:06:33 +0530

    leetcode 869

--- LeetCode/500-1000/Q869 Reordered Power of 2.py
+++ LeetCode/500-1000/Q869 Reordered Power of 2.py
@@ -7,8 +7,18 @@ class Solution:
 
         return ''.join(sorted(list(str(n)))) in powers
 
+    def reorderedPowerOf22(self, n: int) -> bool:
+        sorted_n = ''.join(sorted(list(str(n))))
+        for i in range(31):
+            if ''.join(sorted(list(str(1 << i)))) == sorted_n:
+                return True
+        return False
+
 
 s = Solution()
 print(s.reorderedPowerOf2(1))
 print(s.reorderedPowerOf2(10))
 print(s.reorderedPowerOf2(16))
+print(s.reorderedPowerOf22(1))
+print(s.reorderedPowerOf22(10))
+print(s.reorderedPowerOf22(16))
