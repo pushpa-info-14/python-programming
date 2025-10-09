b03541a677e20751887207e700673955c6312b5f
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-10-09 22:37:41 +0530

    leetcode 3494

--- LeetCode/3000-3500/Q3494 Find The Minimum Amount of Time to Brew Potions.py
+++ LeetCode/3000-3500/Q3494 Find The Minimum Amount of Time to Brew Potions.py
@@ -19,8 +19,23 @@ class Solution:
             gap += mx
         return gap + total_skill * mana[-1]
 
+    def minTime2(self, skill: List[int], mana: List[int]) -> int:
+        n, m = len(skill), len(mana)
+        f = [0] * n
+        for x in mana:
+            now = 0
+            for i in range(n):
+                now = max(now, f[i]) + skill[i] * x
+            f[n - 1] = now
+            for i in range(n - 2, -1, -1):
+                f[i] = f[i + 1] - skill[i + 1] * x
+        return f[n - 1]
+
 
 s = Solution()
 print(s.minTime(skill=[1, 5, 2, 4], mana=[5, 1, 4, 2]))
 print(s.minTime(skill=[1, 1, 1], mana=[1, 1, 1]))
 print(s.minTime(skill=[1, 2, 3, 4], mana=[1, 2]))
+print(s.minTime2(skill=[1, 5, 2, 4], mana=[5, 1, 4, 2]))
+print(s.minTime2(skill=[1, 1, 1], mana=[1, 1, 1]))
+print(s.minTime2(skill=[1, 2, 3, 4], mana=[1, 2]))
