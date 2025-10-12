74bf92e0c6c4d6c57a3750e788c0aa3843f039d9
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-10-13 00:13:59 +0530

    valve shim calculator

---
+++ valve-shim-calculator.py
@@ -0,0 +1,76 @@
+import math
+
+
+class ValveShimCalculator:
+    def __init__(self, readings, c_spec):
+        self._readings = readings
+        self._c_spec = c_spec
+
+    def t_new(self, t_old, c_measured):
+        return t_old + (c_measured - self._c_spec)
+
+    def cal(self, ceil_to_even=False):
+        values = []
+        for c_measured, t_old in self._readings:
+            t = math.ceil(self.t_new(t_old, c_measured) * 100)
+            if ceil_to_even:
+                t += t % 2
+            values.append(t / 100)
+        return values
+
+
+intake_readings = [
+    [0.28, 2.63],
+    [0.28, 2.63],
+    [0.35, 2.63],
+    [0.35, 2.51],
+    [0.38, 2.59],
+    [0.38, 2.61],
+    [0.38, 2.56],
+    [0.38, 2.56]
+]
+exhaust_readings = [
+    [0.40, 2.54],
+    [0.40, 2.44],
+    [0.40, 2.53],
+    [0.40, 2.46],
+    [0.40, 2.46],
+    [0.40, 2.41],
+    [0.48, 2.52],
+    [0.48, 2.47]
+]
+intake = ValveShimCalculator(intake_readings, 0.32)  # 0.32 - 0.40
+exhaust = ValveShimCalculator(exhaust_readings, 0.37)  # 0.37 - 0.45
+res1 = intake.cal(ceil_to_even=True)
+res2 = exhaust.cal(ceil_to_even=True)
+res = res1 + res2
+res.sort()
+print("Intake :", res1)
+print("Exhaust:", res2)
+print("Sorted Req :", res)
+
+ext1 = [i[1] for i in intake_readings]
+ext2 = [i[1] for i in exhaust_readings]
+ext = ext1 + ext2
+ext.sort()
+for i in range(len(ext)):
+    temp = math.ceil(ext[i] * 100)
+    temp += temp % 2
+    ext[i] = temp / 100
+print("Sorted Ext :", ext)
+
+matching = []
+required = []
+n = len(res)
+for i in range(n):
+    found = False
+    for j in range(n):
+        if res[i] == ext[j]:
+            matching.append(res[i])
+            ext[j] = 0
+            found = True
+            break
+    if not found:
+        required.append(res[i])
+print("Matching:", matching)
+print("Required:", required)
