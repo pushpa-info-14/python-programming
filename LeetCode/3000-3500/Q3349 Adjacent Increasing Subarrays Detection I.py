4ef36da4124dfea767587cd03b3b62992e7cbe04
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-10-16 12:47:59 +0530

    leetcode 3349

--- LeetCode/3000-3500/Q3349 Adjacent Increasing Subarrays Detection I.py
+++ LeetCode/3000-3500/Q3349 Adjacent Increasing Subarrays Detection I.py
@@ -16,8 +16,29 @@ class Solution:
                 return True
         return False
 
+    def hasIncreasingSubarrays2(self, nums: List[int], k: int) -> bool:
+        n = len(nums)
+        if n == 2:
+            return True
+        prev = 0
+        cur = 1
+        for i in range(1, n):
+            if nums[i - 1] < nums[i]:
+                cur += 1
+            else:
+                prev = cur
+                cur = 1
+            if (prev >= k and cur >= k) or cur >= 2 * k:
+                return True
+        return False
+
 
 s = Solution()
 print(s.hasIncreasingSubarrays(nums=[2, 5, 7, 8, 9, 2, 3, 4, 3, 1], k=3))
 print(s.hasIncreasingSubarrays(nums=[1, 2, 3, 4, 4, 4, 4, 5, 6, 7], k=5))
 print(s.hasIncreasingSubarrays(nums=[-15, 19], k=1))
+print(s.hasIncreasingSubarrays2(nums=[2, 5, 7, 8, 9, 2, 3, 4, 3, 1], k=3))
+print(s.hasIncreasingSubarrays2(nums=[1, 2, 3, 4, 4, 4, 4, 5, 6, 7], k=5))
+print(s.hasIncreasingSubarrays2(nums=[-15, 19], k=1))
+print(s.hasIncreasingSubarrays2(nums=[-10, 14, 17], k=1))
+print(s.hasIncreasingSubarrays2(nums=[8, -4, -1, 16, 20], k=2))
