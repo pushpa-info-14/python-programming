0170bebc96d98944d2ac911b1650c90b7fcde923
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-10-19 15:45:04 +0530

    leetcode 3397

--- LeetCode/3000-3500/Q3397 Maximum Number of Distinct Elements After Operations.py
+++ LeetCode/3000-3500/Q3397 Maximum Number of Distinct Elements After Operations.py
@@ -1,3 +1,4 @@
+from math import inf
 from typing import List
 
 
@@ -15,8 +16,25 @@ class Solution:
                     break
         return len(seen)
 
+    def maxDistinctElements2(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        res = 0
+        next_available = -inf
+        for num in nums:
+            if num - k >= next_available:
+                next_available = num - k + 1
+                res += 1
+                continue
+            if num - k < next_available <= num + k:
+                next_available += 1
+                res += 1
+        return res
+
 
 s = Solution()
 print(s.maxDistinctElements(nums=[1, 2, 2, 3, 3, 4], k=2))
 print(s.maxDistinctElements(nums=[4, 4, 4, 4], k=1))
-print(s.maxDistinctElements(nums=[56,56,54,54], k=0))
+print(s.maxDistinctElements(nums=[56, 56, 54, 54], k=0))
+print(s.maxDistinctElements2(nums=[1, 2, 2, 3, 3, 4], k=2))
+print(s.maxDistinctElements2(nums=[4, 4, 4, 4], k=1))
+print(s.maxDistinctElements2(nums=[56, 56, 54, 54], k=0))
