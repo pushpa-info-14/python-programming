676b0dca103a610773d2a56e0dada440479d0e01
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-10-19 20:23:17 +0530

    leetcode 1625

---
+++ LeetCode/1500-2000/Q1625 Lexicographically Smallest String After Applying Operations.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findLexSmallestString(self, s: str, a: int, b: int) -> str:
+        odds = []
+        evens = []
+        str_evens = ''
+        n = len(s)
+        for i in range(n):
+            if i & 1:
+                odds.append(int(s[i]))
+            else:
+                evens.append(int(s[i]))
+                str_evens += s[i]
+
+        all_odds = []
+        for i in range(10):
+            all_odds.append(''.join(map(str, ((d + i * a) % 10 for d in odds))))
+
+        all_evens = [str_evens]
+        if b & 1: # If b is odd, even indices will get a chance to increment by a
+            for i in range(10):
+                all_evens.append(''.join(map(str, ((d + i * a) % 10 for d in evens))))
+        res = s
+        for even in all_evens:
+            for odd in all_odds:
+                cur = ''.join(e + o for e, o in zip(even, odd))
+                for i in range(n):
+                    cur = cur[b:] + cur[:b]
+                    res = min(res, cur)
+        return res
+
+
+s = Solution()
+print(s.findLexSmallestString(s="5525", a=9, b=2))  # 2050
+print(s.findLexSmallestString(s="74", a=5, b=1))  # 24
+print(s.findLexSmallestString(s="0011", a=4, b=2))  # 0011
+print(s.findLexSmallestString(s="43987654", a=7, b=3))  # 00553311
