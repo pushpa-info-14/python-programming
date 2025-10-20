9758c68470055038ac6cd4eea78ff444a7bb7386
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-10-20 22:29:39 +0530

    leetcode 2706

---
+++ LeetCode/2500-3000/Q2706 Buy Two Chocolates.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        prices.sort()
+        if money < (prices[0] + prices[1]):
+            return money
+        else:
+            return money - (prices[0] + prices[1])
+
+
+s = Solution()
+print(s.buyChoco(prices=[1, 2, 2], money=3))
+print(s.buyChoco(prices=[3, 2, 3], money=3))
