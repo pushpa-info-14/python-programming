ceeedbeed6dbf2623032391a83691c79678bb9e3
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-10-31 22:45:35 +0530

    leetcode 3289

--- LeetCode/3000-3500/Q3289 The Two Sneaky Numbers of Digitville.py
+++ LeetCode/3000-3500/Q3289 The Two Sneaky Numbers of Digitville.py
@@ -14,8 +14,34 @@ class Solution:
                     break
         return res
 
+    def getSneakyNumbers2(self, nums: List[int]) -> List[int]:
+        n = len(nums) - 2
+        xor = 0
+        for x in range(n):
+            xor ^= x
+        for x in nums:
+            xor ^= x
+        diff_bit = xor & -xor
+        xor1 = 0
+        xor2 = 0
+        for x in range(n):
+            if x & diff_bit:
+                xor1 ^= x
+            else:
+                xor2 ^= x
+        for x in nums:
+            if x & diff_bit:
+                xor1 ^= x
+            else:
+                xor2 ^= x
+
+        return [xor1, xor2]
+
 
 s = Solution()
 print(s.getSneakyNumbers(nums=[0, 1, 1, 0]))
 print(s.getSneakyNumbers(nums=[0, 3, 2, 1, 3, 2]))
 print(s.getSneakyNumbers(nums=[7, 1, 5, 4, 3, 4, 6, 0, 9, 5, 8, 2]))
+print(s.getSneakyNumbers2(nums=[0, 1, 1, 0]))
+print(s.getSneakyNumbers2(nums=[0, 3, 2, 1, 3, 2]))
+print(s.getSneakyNumbers2(nums=[7, 1, 5, 4, 3, 4, 6, 0, 9, 5, 8, 2]))
