a5b47283a50ea60b18c85f0b98ca5662e2b583bf
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-11-01 20:43:00 +0530

    leetcode 2824

---
+++ LeetCode/2500-3000/Q2824 Count Pairs Whose Sum is Less than Target.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        res = 0
+        for i in range(n):
+            pairs = 0
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] < target:
+                    pairs += 1
+            res += pairs
+        return res
+
+
+s = Solution()
+print(s.countPairs(nums=[-1, 1, 2, 3, 1], target=2))
+print(s.countPairs(nums=[-6, 2, 5, -2, -7, -1, 3], target=-2))
+print(s.countPairs(nums=[9, -5, -5, 5, -5, -4, -6, 6, -6], target=3))
