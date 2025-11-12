f8080d7c53245cb1d06e6cfed091f673d2729262
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-11-12 21:20:26 +0530

    leetcode 2654

---
+++ LeetCode/2500-3000/Q2654 Minimum Number of Operations to Make All Array Elements Equal to 1.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        n = len(nums)
+        one_count = 0
+        for num in nums:
+            if num == 1:
+                one_count += 1
+        if one_count > 0:
+            return n - one_count
+
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return abs(a)
+
+        min_sub_len = 10 ** 10
+        for l in range(n):
+            cur_gcd = 0
+            for r in range(l, n):
+                if r - l + 1 >= min_sub_len:
+                    break
+                cur_gcd = gcd(cur_gcd, nums[r])
+                if cur_gcd == 1:
+                    min_sub_len = r - l + 1
+                    break
+
+        if min_sub_len == 10 ** 10:
+            return -1
+        return min_sub_len - 1 + n - 1
+
+
+s = Solution()
+print(s.minOperations(nums=[2, 6, 3, 4]))
+print(s.minOperations(nums=[2, 10, 6, 14]))
