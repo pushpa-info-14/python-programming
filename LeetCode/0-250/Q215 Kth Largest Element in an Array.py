fd0a4e1a10870c3a757ac461cd2144ff197089fb
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-11-23 21:58:37 +0530

    leetcode cleanups

--- LeetCode/0-250/Q215 Kth Largest Element in an Array.py
+++ LeetCode/0-250/Q215 Kth Largest Element in an Array.py
@@ -1,8 +1,19 @@
+import heapq
 from typing import List
 
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        q = []
+
+        for num in nums:
+            heapq.heappush(q, -num)
+        res = 0
+        for i in range(k):
+            res = -(heapq.heappop(q))
+        return res
+
+    def findKthLargest2(self, nums: List[int], k: int) -> int:
         k = len(nums) - k
 
         def quick_select(l, r):
@@ -23,7 +34,8 @@ class Solution:
         return quick_select(0, len(nums) - 1)
 
 
-# Quick Select
 s = Solution()
 print(s.findKthLargest([3, 2, 1, 5, 6, 4], 2))
 print(s.findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
+print(s.findKthLargest2([3, 2, 1, 5, 6, 4], 2))
+print(s.findKthLargest2([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
--- LeetCode/0-250/Q75 Sort Colors.py
+++ LeetCode/0-250/Q75 Sort Colors.py
@@ -6,6 +6,26 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        buckets = [0, 0, 0]
+
+        for i in nums:
+            buckets[i] += 1
+        k = 0
+        for i in range(len(buckets)):
+            for j in range(buckets[i]):
+                if i == 0:
+                    nums[k] = 0
+                elif i == 1:
+                    nums[k] = 1
+                else:
+                    nums[k] = 2
+                k += 1
+        print(nums)
+
+    def sortColors2(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         l, r = 0, len(nums) - 1
         i = 0
 
@@ -24,8 +44,6 @@ class Solution:
 
         print(nums)
 
-
-# QuickSort
-# Partition
 s = Solution()
 print(s.sortColors([2, 0, 2, 1, 1, 0]))
+print(s.sortColors2([2, 0, 2, 1, 1, 0]))
