f91a62818adedf4c8ee103161ca2b10a9cedbc04
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-11-23 22:45:16 +0530

    leetcode cleanups

--- LeetCode/2000-2500/Q2127 Maximum Employees to Be Invited to a Meeting.py
+++ LeetCode/2000-2500/Q2127 Maximum Employees to Be Invited to a Meeting.py
@@ -53,8 +53,47 @@ class Solution:
             chain_sum += bfs(n1, n2) + bfs(n2, n1) + 2
         return max(chain_sum, longest_cycle)
 
+    def maximumInvitations2(self, favorite: List[int]) -> int:
+        n, longest_cycle, two_cycle_invitations = len(favorite), 0, 0
+        in_degree = [0 for _ in range(n)]
+        depth = [1 for _ in range(n)]
+
+        for person in range(n):
+            in_degree[favorite[person]] += 1
+
+        q = deque()
+        for person in range(n):
+            if in_degree[person] == 0:
+                q.append(person)
+
+        while q:
+            curr = q.popleft()
+            next = favorite[curr]
+            depth[next] = max(depth[next], depth[curr] + 1)
+            in_degree[next] -= 1
+            if in_degree[next] == 0:
+                q.append(next)
+
+        # only have cycles
+        for person in range(n):
+            if in_degree[person] == 0: continue
+            cycle_length = 0
+            curr = person
+            while in_degree[curr] != 0:
+                in_degree[curr] = 0
+                cycle_length += 1
+                curr = favorite[curr]
+            if cycle_length == 2:
+                two_cycle_invitations += depth[person] + depth[favorite[person]]
+            else:
+                longest_cycle = max(longest_cycle, cycle_length)
+
+        return max(longest_cycle, two_cycle_invitations)
 
 s = Solution()
 print(s.maximumInvitations([2, 2, 1, 2]))
 print(s.maximumInvitations([1, 2, 0]))
 print(s.maximumInvitations([3, 0, 1, 4, 1]))
+print(s.maximumInvitations2([2, 2, 1, 2]))
+print(s.maximumInvitations2([1, 2, 0]))
+print(s.maximumInvitations2([3, 0, 1, 4, 1]))
--- LeetCode/2000-2500/Q2290 Minimum Obstacle Removal To Reach Corner.py
+++ LeetCode/2000-2500/Q2290 Minimum Obstacle Removal To Reach Corner.py
@@ -1,3 +1,4 @@
+import heapq
 from collections import deque
 from typing import List
 
@@ -27,7 +28,30 @@ class Solution:
                     q.appendleft((obstacles, newR, newC))
                 visited.add((newR, newC))
 
+    def minimumObstacles2(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        q = [(0, 0, 0)]  # (obstacles, r, c)
+        visited = {(0, 0)}
+
+        while q:
+            obstacles, r, c = heapq.heappop(q)
+
+            if (r, c) == (m - 1, n - 1):
+                return obstacles
+            for i in range(4):
+                newR = r + directions[i][0]
+                newC = c + directions[i][1]
+
+                if (newR, newC) in visited or newR < 0 or newC < 0 or newR == m or newC == n:
+                    continue
+                heapq.heappush(q, (obstacles + grid[newR][newC], newR, newC))
+                visited.add((newR, newC))
+
 
 solution = Solution()
 print(solution.minimumObstacles([[0, 1, 1], [1, 1, 0], [1, 1, 0]]))  # 2
 print(solution.minimumObstacles([[0, 1, 0, 0, 0], [0, 1, 0, 1, 0], [0, 0, 0, 1, 0]]))  # 0
+print(solution.minimumObstacles2([[0, 1, 1], [1, 1, 0], [1, 1, 0]]))  # 2
+print(solution.minimumObstacles2([[0, 1, 0, 0, 0], [0, 1, 0, 1, 0], [0, 0, 0, 1, 0]]))  # 0
