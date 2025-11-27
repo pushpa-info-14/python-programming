3ac38bf523d81ed0f5ce0188e4e883d111ed8304
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-11-27 19:29:58 +0530

    leetcode 2670

---
+++ LeetCode/2500-3000/Q2670 Find the Distinct Difference Array.py
@@ -0,0 +1,23 @@
+from collections import defaultdict, Counter
+from typing import List
+
+
+class Solution:
+    def distinctDifferenceArray(self, nums: List[int]) -> List[int]:
+        prefix = set()
+        suffix = Counter(nums)
+        initial = len(suffix)
+        res = []
+        for num in nums:
+            prefix.add(num)
+            suffix[num] -= 1
+            if suffix[num] == 0:
+                del suffix[num]
+                initial -= 1
+            res.append(len(prefix) - initial)
+        return res
+
+
+s = Solution()
+print(s.distinctDifferenceArray(nums=[1, 2, 3, 4, 5]))
+print(s.distinctDifferenceArray(nums=[3, 2, 3, 4, 2]))
