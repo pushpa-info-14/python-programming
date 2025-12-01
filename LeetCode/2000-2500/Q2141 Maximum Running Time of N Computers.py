b253e187bd553b861b120a1583b48c2ed5e6bd39
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-12-01 18:34:39 +0530

    leetcode 2141

---
+++ LeetCode/2000-2500/Q2141 Maximum Running Time of N Computers.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def maxRunTime(self, n: int, batteries: List[int]) -> int:
+        def check(target):
+            max_minutes = 0
+            for x in batteries:
+                max_minutes += min(target, x)
+            return max_minutes >= n * target
+
+        low, high = 0, sum(batteries) // n
+        res = high
+        while low <= high:
+            mid = (low + high) // 2
+            if check(mid):
+                low = mid + 1
+                res = mid
+            else:
+                high = mid - 1
+        return res
+
+
+s = Solution()
+print(s.maxRunTime(n=2, batteries=[3, 3, 3]))  # 4
+print(s.maxRunTime(n=2, batteries=[1, 1, 1, 1]))  # 2
+print(s.maxRunTime(n=3, batteries=[10, 10, 6, 9, 3]))  # 12
+print(s.maxRunTime(n=3, batteries=[10, 10, 3, 5]))  # 8
