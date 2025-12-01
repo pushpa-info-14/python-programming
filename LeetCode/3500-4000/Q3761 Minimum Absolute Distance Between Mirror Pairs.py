608d95bfb1a8f454688b5c5e904b5d1b6ef2f6d4
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-12-01 09:40:05 +0530

    leetcode 3761

---
+++ LeetCode/3500-4000/Q3761 Minimum Absolute Distance Between Mirror Pairs.py
@@ -0,0 +1,33 @@
+import math
+from collections import defaultdict
+from typing import List
+
+
+def reverse(x):
+    num = 0
+    while x:
+        num *= 10
+        num += x % 10
+        x //= 10
+    return num
+
+
+class Solution:
+    def minMirrorPairDistance(self, nums: List[int]) -> int:
+        n = len(nums)
+        mp = defaultdict(list)
+        res = math.inf
+        for i in range(n):
+            num = nums[i]
+            if num in mp:
+                j = mp[num][-1]
+                res = min(res, abs(i - j))
+            x = reverse(num)
+            mp[x].append(i)
+        return res if res != math.inf else -1
+
+
+s = Solution()
+print(s.minMirrorPairDistance(nums=[12, 21, 45, 33, 54]))
+print(s.minMirrorPairDistance(nums=[120, 21]))
+print(s.minMirrorPairDistance(nums=[21, 120]))
