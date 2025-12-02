02e81c26451e81352a86c82ad39101fad15d2e71
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-12-02 20:35:02 +0530

    leetcode 3623

---
+++ LeetCode/3500-4000/Q3623 Count Number of Trapezoids I.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def countTrapezoids(self, points: List[List[int]]) -> int:
+        mod = 10 ** 9 + 7
+        y_planes = defaultdict(int)
+        for x, y in points:
+            y_planes[y] += 1
+        pairs = []
+        for n in y_planes.values():
+            if n > 1:
+                pairs.append((n * (n - 1)) // 2)
+        total_pairs = sum(pairs)
+        res = 0
+        for x in pairs:
+            res += x * (total_pairs - x)
+        res //= 2
+        return res % mod
+
+
+s = Solution()
+print(s.countTrapezoids(points=[[1, 0], [2, 0], [3, 0], [2, 2], [3, 2]]))
+print(s.countTrapezoids(points=[[0, 0], [1, 0], [0, 1], [2, 1]]))
