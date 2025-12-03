606c3142ffe34ed43e2eaaf5489876257e7fc93f
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-12-03 20:22:47 +0530

    leetcode 3623

--- LeetCode/3500-4000/Q3623 Count Number of Trapezoids I.py
+++ LeetCode/3500-4000/Q3623 Count Number of Trapezoids I.py
@@ -19,7 +19,22 @@ class Solution:
         res //= 2
         return res % mod
 
+    def countTrapezoids2(self, points: List[List[int]]) -> int:
+        mod = 10 ** 9 + 7
+        intercepts = defaultdict(int)
+        for point in points:
+            intercepts[point[1]] += 1
+        total_sum = 0
+        res = 0
+        for x in intercepts.values():
+            edge = x * (x - 1) // 2
+            res = (res + edge * total_sum) % mod
+            total_sum = (total_sum + edge) % mod
+        return res
+
 
 s = Solution()
 print(s.countTrapezoids(points=[[1, 0], [2, 0], [3, 0], [2, 2], [3, 2]]))
 print(s.countTrapezoids(points=[[0, 0], [1, 0], [0, 1], [2, 1]]))
+print(s.countTrapezoids2(points=[[1, 0], [2, 0], [3, 0], [2, 2], [3, 2]]))
+print(s.countTrapezoids2(points=[[0, 0], [1, 0], [0, 1], [2, 1]]))
