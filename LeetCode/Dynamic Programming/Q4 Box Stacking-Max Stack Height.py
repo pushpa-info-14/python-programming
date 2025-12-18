cc5b6d5400202f9a1b5376c63275ce35749d8b0c
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-12-18 13:37:06 +0530

    dp box stacking

--- LeetCode/Dynamic Programming/Q4 Box Stacking-Max Stack Height.py
+++ LeetCode/Dynamic Programming/Q4 Box Stacking-Max Stack Height.py
@@ -2,17 +2,6 @@ from typing import List
 
 
 class Solution:
-    def tallest_possible_stack_size(self, boxes: List[List[int]]) -> int:
-        n = len(boxes)
-        boxes.sort()
-        dp = [1] * n
-        for i in range(1, n):  # Starts from 1 and look back
-            for j in range(i):
-                if boxes[j][0] < boxes[i][0] and boxes[j][1] < boxes[i][1]:
-                    dp[i] = max(dp[i], 1 + dp[j])
-        print(dp)
-        return dp[n - 1]
-
     def tallest_possible_stack_height(self, boxes: List[List[int]]) -> int:
         n = len(boxes)
         boxes.sort()
@@ -31,5 +20,4 @@ height of the tallest possible stack. Box [Li,Wi,Hi] can be on top of box [Lj,Wj
 """
 
 s = Solution()
-print(s.tallest_possible_stack_size([[4, 5, 3], [2, 3, 2], [3, 6, 2], [1, 5, 4], [2, 4, 1], [1, 2, 2]]))
 print(s.tallest_possible_stack_height([[4, 5, 3], [2, 3, 2], [3, 6, 2], [1, 5, 4], [2, 4, 1], [1, 2, 2]]))
