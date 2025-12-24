d5e96758e078e39cb292f01ceffa7d1dab3b7663
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-12-24 10:51:06 +0530

    dp shortest common supersequence

--- Data Structures & Algorithms/DSA Course/Step-16-Dynamic Programming/L31 Shortest Common Supersequence - DP on Strings.py
+++ Data Structures & Algorithms/DSA Course/Step-16-Dynamic Programming/L31 Shortest Common Supersequence - DP on Strings.py
@@ -4,12 +4,12 @@ class Solution:
         n = len(str2)
         dp = [[0] * (n + 1) for _ in range(m + 1)]
 
-        for i in range(m):
-            for j in range(n):
-                if str1[i] == str2[j]:
-                    dp[i + 1][j + 1] = 1 + dp[i][j]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if str1[i - 1] == str2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
                 else:
-                    dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1])
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
 
         ans = ""
         i, j = m, n
@@ -34,6 +34,7 @@ class Solution:
         return ans[::-1]
 
 
+# length = m + n - len(lcs)
 # LeetCode 1092
 s = Solution()
 print(s.shortestCommonSupersequence("abac", "cab"))
