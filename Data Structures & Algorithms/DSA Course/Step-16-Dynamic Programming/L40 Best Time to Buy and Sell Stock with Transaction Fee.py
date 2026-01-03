b4358678eeddb0b227f8c219f5309f6561c6d293
pushpa.info.14 <pushpa.info.14@gmail.com>
2026-01-03 19:11:32 +0530

    dp best time to buy and sell stock with transaction fee

--- Data Structures & Algorithms/DSA Course/Step-16-Dynamic Programming/L40 Best Time to Buy and Sell Stock with Transaction Fee.py
+++ Data Structures & Algorithms/DSA Course/Step-16-Dynamic Programming/L40 Best Time to Buy and Sell Stock with Transaction Fee.py
@@ -2,26 +2,24 @@ from typing import List
 
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
         n = len(prices)
         memo = {}
 
         def dfs(i, can_buy):
-            if i >= n:
+            if i == n:
                 return 0
-
             if (i, can_buy) in memo:
                 return memo[(i, can_buy)]
             if can_buy:
                 memo[(i, can_buy)] = max(dfs(i + 1, True), -prices[i] + dfs(i + 1, False))
             else:
-                memo[(i, can_buy)] = max(dfs(i + 1, False), prices[i] + dfs(i + 2, True))
+                memo[(i, can_buy)] = max(dfs(i + 1, False), prices[i] - fee + dfs(i + 1, True))
             return memo[(i, can_buy)]
 
         return dfs(0, True)
 
 
-# LeetCode 309
 s = Solution()
-print(s.maxProfit(prices=[1, 2, 3, 0, 2]))
-print(s.maxProfit(prices=[1]))
+print(s.maxProfit(prices=[1, 3, 2, 8, 4, 9], fee=2))
+print(s.maxProfit(prices=[1, 3, 7, 5, 10, 3], fee=3))
