3daaa53f929230f2c6b5f488cc8a136b7ae49b73
pushpa.info.14 <pushpa.info.14@gmail.com>
2026-01-10 14:10:41 +0530

    leetcode 712

--- LeetCode/500-750/Q712 Minimum ASCII Delete Sum for Two Strings.py
+++ LeetCode/500-750/Q712 Minimum ASCII Delete Sum for Two Strings.py
@@ -24,7 +24,27 @@ class Solution:
 
         return dfs(0, 0)
 
+    def minimumDeleteSum2(self, s1: str, s2: str) -> int:
+        dp = [[0] * (len(s1) + 1) for _ in range(len(s2) + 1)]
+        # min sum = sum(ord chars) - ord(max string that is equal)
+        for i in range(len(s2) - 1, -1, - 1):
+            for j in range(len(s1) - 1, -1, -1):
+                if s2[i] == s1[j]:
+                    dp[i][j] = ord(s2[i]) + dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+        sum_s1 = 0
+        sum_s2 = 0
+        for c in s1:
+            sum_s1 += ord(c)
+        for c in s2:
+            sum_s2 += ord(c)
+        return sum_s1 + sum_s2 - 2 * dp[0][0]
+
 
 s = Solution()
 print(s.minimumDeleteSum(s1="sea", s2="eat"))
 print(s.minimumDeleteSum(s1="delete", s2="leet"))
+print("---------------------------")
+print(s.minimumDeleteSum2(s1="sea", s2="eat"))
+print(s.minimumDeleteSum2(s1="delete", s2="leet"))
