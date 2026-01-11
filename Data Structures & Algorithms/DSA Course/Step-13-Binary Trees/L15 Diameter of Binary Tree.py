83a19caed2680e53806cc0720d593e9653821424
pushpa.info.14 <pushpa.info.14@gmail.com>
2026-01-11 11:55:05 +0530

    cleanups

--- Data Structures & Algorithms/DSA Course/Step-13-Binary Trees/L15 Diameter of Binary Tree.py
+++ Data Structures & Algorithms/DSA Course/Step-13-Binary Trees/L15 Diameter of Binary Tree.py
@@ -5,20 +5,21 @@ from Common.TreeNode import TreeNode
 
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        diameter = [0]
+        diameter = 0
 
         def dfs(node):
+            nonlocal diameter
             if not node:
                 return 0
             dl = dfs(node.left)
             dr = dfs(node.right)
-            diameter[0] = max(diameter[0], dl + dr)
+            diameter = max(diameter, dl + dr)
             return 1 + max(dl, dr)
 
         dfs(root)
-        return diameter[0]
+        return diameter
 
-
+# LeetCode 543
 tree = TreeNode.build([1, 2, 3, 4, 5, 6, 7])
 
 s = Solution()
--- LeetCode/0-250/Q124 Binary Tree Maximum Path Sum.py
+++ LeetCode/0-250/Q124 Binary Tree Maximum Path Sum.py
@@ -5,19 +5,20 @@ from Common.TreeNode import TreeNode
 
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        res = [root.val]
+        res = root.val
 
         def dfs(node):
+            nonlocal res
             if not node:
                 return 0
             l = max(dfs(node.left), 0)
             r = max(dfs(node.right), 0)
-            res[0] = max(res[0], l + r + node.val)
+            res = max(res, l + r + node.val)
 
             return node.val + max(l, r)
 
         dfs(root)
-        return res[0]
+        return res
 
 
 tree = TreeNode.build([1, 2, 3, 4, 5, 6, 7])
