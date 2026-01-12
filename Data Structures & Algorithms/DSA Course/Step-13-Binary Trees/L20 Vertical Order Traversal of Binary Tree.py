fb14ce8d2951737b9da2a4df50e4016aa6b71945
pushpa.info.14 <pushpa.info.14@gmail.com>
2026-01-12 21:04:16 +0530

    vertical order traversal of binary tree

--- Data Structures & Algorithms/DSA Course/Step-13-Binary Trees/L20 Vertical Order Traversal of Binary Tree.py
+++ Data Structures & Algorithms/DSA Course/Step-13-Binary Trees/L20 Vertical Order Traversal of Binary Tree.py
@@ -2,8 +2,37 @@ from collections import deque, defaultdict
 
 from Common.BinaryTreeNode import BinaryTreeNode
 
+"""
+Note: If there are multiple nodes passing through a vertical line, then they should be printed as they 
+appear in level order traversal of the tree.
+"""
 
-def verticalOrderTraversal(root):
+
+def verticalOrderTraversalModerate(root):
+    mp = defaultdict(list)
+    q = deque([(0, 0, root)])
+
+    while q:
+        for _ in range(len(q)):
+            x, y, node = q.popleft()
+            mp[x].append(node.data)
+            if node.left:
+                q.append((x - 1, y + 1, node.left))
+            if node.right:
+                q.append((x + 1, y + 1, node.right))
+
+    res = []
+    for i in sorted(mp.keys()):
+        res += mp[i]
+    return res
+
+
+"""
+If multiple nodes have the same 'x' and 'y' coordinates, they will be accessed in non-decreasing order of values.
+"""
+
+
+def verticalOrderTraversalHard(root):
     q = deque([(0, 0, root)])
     mp = defaultdict(list)
 
@@ -54,7 +83,8 @@ class Solution:
 
 
 tree = BinaryTreeNode.build([2, 7, 5, 2, 6, None, 9, None, None, 5, 11, 4, None, None, None, None, None, None, None])
-print(verticalOrderTraversal(tree))
+print(verticalOrderTraversalModerate(tree))
+print(verticalOrderTraversalHard(tree))
 
 s = Solution()
 print(s.verticalOrder(tree))
