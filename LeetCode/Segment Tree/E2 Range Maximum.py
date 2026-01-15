5d38258ade475c2e5697338f7fdb67b65e8817e3
pushpa.info.14 <pushpa.info.14@gmail.com>
2026-01-15 20:36:05 +0530

    ST range min and max

---
+++ LeetCode/Segment Tree/E2 Range Maximum.py
@@ -0,0 +1,62 @@
+from typing import List
+
+
+class SegmentTree:
+    _inf = 10 ** 10
+
+    def __init__(self, arr):
+        self.n = len(arr)
+        self.tree = [0] * (4 * self.n)
+        self._build(0, 0, self.n - 1, arr)
+
+    def _build(self, node, low, high, arr):
+        if low == high:
+            self.tree[node] = arr[low]
+        else:
+            mid = (low + high) // 2
+            self._build(2 * node + 1, low, mid, arr)
+            self._build(2 * node + 2, mid + 1, high, arr)
+            self.tree[node] = max(self.tree[2 * node + 1], self.tree[2 * node + 2])
+
+    def _query(self, node, low, high, l, r):
+        # No overlap [l r low high] [low high l r]
+        if r < low or high < l:
+            return -self._inf
+        # Complete overlap [l low high r]
+        if l <= low and high <= r:
+            return self.tree[node]
+        # Partial overlap
+        mid = (low + high) // 2
+        left = self._query(2 * node + 1, low, mid, l, r)
+        right = self._query(2 * node + 2, mid + 1, high, l, r)
+        return max(left, right)
+
+    def _update(self, node, low, high, index, val):
+        if low == high:
+            self.tree[node] = val
+        else:
+            mid = (low + high) // 2
+            if index <= mid:
+                self._update(2 * node + 1, low, mid, index, val)
+            else:
+                self._update(2 * node + 2, mid + 1, high, index, val)
+            self.tree[node] = max(self.tree[2 * node + 1], self.tree[2 * node + 2])
+
+    def query(self, l, r):
+        return self._query(0, 0, self.n - 1, l, r)
+
+    def update(self, index, value):
+        self._update(0, 0, self.n - 1, index, value)
+
+
+class Solution:
+    def rangeMax(self, nums: List[int], queries: List[List[int]]) -> List[int]:
+        st = SegmentTree(nums)
+        res = []
+        for l, r in queries:
+            res.append(st.query(l, r))
+        return res
+
+
+s = Solution()
+print(s.rangeMax([1, 2, 7, 1, 6, 3, 5], [[1, 4], [3, 4], [1, 2], [1, 5]]))
