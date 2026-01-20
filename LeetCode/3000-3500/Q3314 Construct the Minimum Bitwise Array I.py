99a44258118d5e7005cf582b710e51740e92dec9
pushpa.info.14 <pushpa.info.14@gmail.com>
2026-01-20 08:42:35 +0530

    leetcode 3314

--- LeetCode/3000-3500/Q3314 Construct the Minimum Bitwise Array I.py
+++ LeetCode/3000-3500/Q3314 Construct the Minimum Bitwise Array I.py
@@ -3,34 +3,14 @@ from typing import List
 
 class Solution:
     def minBitwiseArray(self, nums: List[int]) -> List[int]:
-        """
-        1011 - 11
-        1001 - 9
-        1010 - 10 9 | 10 = 11
-
-        1101 - 13
-        1100 - 12
-        1101 - 13 12 | 13 = 13
-
-        11111 - 31
-         1111 - 15
-        10000 - 16 15 | 16 = 31
-        """
         res = []
         for num in nums:
-            if num == 2:
-                res.append(-1)
-                continue
-
-            b = bin(num).lstrip('0b')
-            set_bits = b.count('1')
-            if len(b) == set_bits:
-                res.append(int('1' * (set_bits - 1), 2))
-            else:
-                idx = b.rfind('0')
-                digits = list(b)
-                digits[idx + 1] = '0'
-                res.append(int(''.join(digits), 2))
+            cur = - 1
+            for candidate in range(num):
+                if candidate | (candidate + 1) == num:
+                    cur = candidate
+                    break
+            res.append(cur)
         return res
 
 
