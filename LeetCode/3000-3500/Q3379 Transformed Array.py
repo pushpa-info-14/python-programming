1d630d91784c1be69bf5349cd758fa80b7e1da89
pushpa.info.14 <pushpa.info.14@gmail.com>
2026-02-05 20:06:58 +0530

    leetcode 3379

--- LeetCode/3000-3500/Q3379 Transformed Array.py
+++ LeetCode/3000-3500/Q3379 Transformed Array.py
@@ -5,8 +5,8 @@ class Solution:
     def constructTransformedArray(self, nums: List[int]) -> List[int]:
         n = len(nums)
         res = [0] * n
-        for i, num in enumerate(nums):
-            res[i] = nums[(i + num) % n]
+        for i in range(n):
+            res[i] = nums[(i + nums[i]) % n]
         return res
 
 
