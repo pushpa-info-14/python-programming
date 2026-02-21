0d28d736cbe8bfe7c4cf8c6ca1d3a1b10ed2e371
pushpa.info.14 <pushpa.info.14@gmail.com>
2026-02-21 22:15:48 +0530

    leetcode 762

--- LeetCode/750-1000/Q762 Prime Number of Set Bits in Binary Representation.py
+++ LeetCode/750-1000/Q762 Prime Number of Set Bits in Binary Representation.py
@@ -1,6 +1,3 @@
-import math
-
-
 class Solution:
     def countPrimeSetBits(self, left: int, right: int) -> int:
         primes = {2, 3, 5, 7, 11, 13, 17, 19}  # Max bit count math.log2(10 ** 6)
@@ -14,7 +11,22 @@ class Solution:
                 res += 1
         return res
 
+    def countPrimeSetBits2(self, left: int, right: int) -> int:
+        primes = {2, 3, 5, 7, 11, 13, 17, 19}  # Max bit count math.log2(10 ** 6)
+        res = 0
+        for num in range(left, right + 1):
+            bits = 0
+            while num:
+                bits += 1
+                num = num & (num - 1)
+            if bits in primes:
+                res += 1
+        return res
+
 
 s = Solution()
 print(s.countPrimeSetBits(left=6, right=10))
 print(s.countPrimeSetBits(left=10, right=15))
+print("-------------")
+print(s.countPrimeSetBits2(left=6, right=10))
+print(s.countPrimeSetBits2(left=10, right=15))
