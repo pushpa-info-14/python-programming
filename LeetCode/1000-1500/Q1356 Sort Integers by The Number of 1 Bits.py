2a1c4dd3fef9982dc8d879e0dd2f8d72a69c23fd
pushpa.info.14 <pushpa.info.14@gmail.com>
2026-02-25 10:32:20 +0530

    leetcode 1356

--- LeetCode/1000-1500/Q1356 Sort Integers by The Number of 1 Bits.py
+++ LeetCode/1000-1500/Q1356 Sort Integers by The Number of 1 Bits.py
@@ -1,24 +1,11 @@
-from collections import defaultdict
 from typing import List
 
 
 class Solution:
     def sortByBits(self, arr: List[int]) -> List[int]:
         arr.sort()
-        mp = defaultdict(list)
-
-        for num in arr:
-            x = num
-            bits = 0
-            while x:
-                bits += 1
-                x &= x - 1
-            mp[bits].append(num)
-
-        res = []
-        for key in sorted(mp.keys()):
-            res += mp[key]
-        return res
+        arr.sort(key=lambda x: x.bit_count())
+        return arr
 
 
 s = Solution()
