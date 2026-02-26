66ffdaacddecdd1863f0e29518d7dfb18651d997
pushpa.info.14 <pushpa.info.14@gmail.com>
2026-02-26 14:13:50 +0530

    leetcode 1404

--- LeetCode/1000-1500/Q1404 Number of Steps to Reduce a Number in Binary Representation to One.py
+++ LeetCode/1000-1500/Q1404 Number of Steps to Reduce a Number in Binary Representation to One.py
@@ -10,8 +10,24 @@ class Solution:
                 num //= 2
         return res
 
+    def numSteps2(self, s: str) -> int:
+        res = 0
+        carry = 0
+        for i in reversed(range(1, len(s))):
+            bit = int(s[i]) + carry
+            if bit % 2:
+                res += 2
+                carry = 1
+            else:
+                res += 1
+        return res + carry
+
 
 s = Solution()
 print(s.numSteps(s="1101"))
 print(s.numSteps(s="10"))
 print(s.numSteps(s="1"))
+print("--------------")
+print(s.numSteps2(s="1101"))
+print(s.numSteps2(s="10"))
+print(s.numSteps2(s="1"))
