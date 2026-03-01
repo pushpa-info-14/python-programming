580109e38107546050665be85caf2d15e30d7f4c
pushpa.info.14 <pushpa.info.14@gmail.com>
2026-03-01 20:16:21 +0530

    leetcode 1689

---
+++ LeetCode/1500-2000/Q1689 Partitioning Into Minimum Number Of Deci-Binary Numbers.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        return int(max(n))
+
+
+s = Solution()
+print(s.minPartitions("32"))
+print(s.minPartitions("82734"))
+print(s.minPartitions("27346209830709182346"))
