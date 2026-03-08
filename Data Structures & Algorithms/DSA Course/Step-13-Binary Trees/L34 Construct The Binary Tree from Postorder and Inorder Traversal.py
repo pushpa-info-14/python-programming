e4319d148783ce83b3bb9198e4467a9594626838
pushpa.info.14 <pushpa.info.14@gmail.com>
2026-03-08 21:19:00 +0530

    leetcode 106 optimized

--- Data Structures & Algorithms/DSA Course/Step-13-Binary Trees/L34 Construct The Binary Tree from Postorder and Inorder Traversal.py
+++ Data Structures & Algorithms/DSA Course/Step-13-Binary Trees/L34 Construct The Binary Tree from Postorder and Inorder Traversal.py
@@ -7,14 +7,35 @@ class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
         if len(inorder) == 0:
             return None
-        root_index = inorder.index(postorder[-1])
+        i_root = inorder.index(postorder[-1])
         return TreeNode(
             postorder[-1],
-            self.buildTree(inorder[:root_index], postorder[:root_index]),
-            self.buildTree(inorder[root_index + 1:], postorder[root_index: -1])
+            self.buildTree(inorder[:i_root], postorder[:i_root]),
+            self.buildTree(inorder[i_root + 1:], postorder[i_root: -1])
         )
+
+    def buildTree2(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        mp = {}
+        for i in range(len(inorder)):
+            mp[inorder[i]] = i
+
+        def dfs(il, ir, pl, pr):
+            if il > ir or pl > pr:
+                return None
+            i_root = mp[postorder[pr]]
+            nums_left = i_root - il
+            return TreeNode(
+                postorder[pr],
+                dfs(il, i_root - 1, pl, pl + nums_left - 1),
+                dfs(i_root + 1, ir, pl + nums_left, pr - 1)
+            )
+
+        n = len(inorder)
+        return dfs(0, n - 1, 0, n - 1)
 
 
 s = Solution()
 s.buildTree(inorder=[9, 3, 15, 20, 7], postorder=[9, 15, 7, 20, 3]).preorder_traversal()
+s.buildTree2(inorder=[9, 3, 15, 20, 7], postorder=[9, 15, 7, 20, 3]).preorder_traversal()
 s.buildTree(inorder=[-1], postorder=[-1]).preorder_traversal()
+s.buildTree2(inorder=[-1], postorder=[-1]).preorder_traversal()
