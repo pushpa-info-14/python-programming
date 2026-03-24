d0cbbab3c10649b3b95696f7a7946c2b012e803a
pushpa.info.14 <pushpa.info.14@gmail.com>
2026-03-24 10:30:10 +0530

    leetcode 3875

---
+++ LeetCode/3750-4000/Q3875 Construct Uniform Parity Array I.py
@@ -0,0 +1,8 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
+
+
+s = Solution()
+print(s.uniformArray(nums1=[2, 3]))
+print(s.uniformArray(nums1=[4, 6]))
