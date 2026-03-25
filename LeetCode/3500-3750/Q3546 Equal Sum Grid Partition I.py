509f1a8bff04b4819a1d9b6f2c9c0734c46f4ece
pushpa.info.14 <pushpa.info.14@gmail.com>
2026-03-25 10:03:46 +0530

    leetcode 3546

---
+++ LeetCode/3500-3750/Q3546 Equal Sum Grid Partition I.py
@@ -0,0 +1,45 @@
+from itertools import accumulate
+from typing import List
+
+
+class Solution:
+    def canPartitionGrid(self, grid: List[List[int]]) -> bool:
+        m = len(grid)
+        n = len(grid[0])
+        pre_sum = [[0] * (n + 1) for _ in range(m + 1)]
+        for r in range(m):
+            for c in range(n):
+                pre_sum[r + 1][c + 1] = grid[r][c] + pre_sum[r + 1][c] + pre_sum[r][c + 1] - pre_sum[r][c]
+        for r in range(1, m + 1):
+            if pre_sum[r][-1] == pre_sum[-1][-1] - pre_sum[r][-1]:
+                return True
+        for c in range(1, n + 1):
+            if pre_sum[-1][c] == pre_sum[-1][-1] - pre_sum[-1][c]:
+                return True
+        return False
+
+    def canPartitionGrid2(self, grid: List[List[int]]) -> bool:
+        m = len(grid)
+        n = len(grid[0])
+        row_sums = [0] * m
+        col_sums = [0] * n
+        total = 0
+        for r in range(m):
+            for c in range(n):
+                row_sums[r] += grid[r][c]
+                col_sums[c] += grid[r][c]
+                total += grid[r][c]
+
+        return any(
+            s == total / 2
+            for sums in (row_sums, col_sums)
+            for s in accumulate(sums)
+        )
+
+
+s = Solution()
+print(s.canPartitionGrid(grid=[[1, 4], [2, 3]]))
+print(s.canPartitionGrid(grid=[[1, 3], [2, 4]]))
+print("----------")
+print(s.canPartitionGrid2(grid=[[1, 4], [2, 3]]))
+print(s.canPartitionGrid2(grid=[[1, 3], [2, 4]]))
