0f6ada1ae92e22d2bb7cda9ee5f8c70bef9072c4
pushpa.info.14 <pushpa.info.14@gmail.com>
2026-04-05 12:52:46 +0530

    leetcode 657

--- LeetCode/500-750/Q657 Robot Return to Origin.py
+++ LeetCode/500-750/Q657 Robot Return to Origin.py
@@ -3,18 +3,8 @@ from collections import Counter
 
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        directions = {
-            'R': [-1, 0],
-            'L': [1, 0],
-            'U': [0, 1],
-            'D': [0, -1]
-        }
-        x, y = 0, 0
-        for key, value in Counter(moves).items():
-            dx, dy = directions[key]
-            x += dx * value
-            y += dy * value
-        return x == 0 and y == 0
+        counter = Counter(moves)
+        return counter["L"] == counter["R"] and counter["U"] == counter["D"]
 
 
 s = Solution()
