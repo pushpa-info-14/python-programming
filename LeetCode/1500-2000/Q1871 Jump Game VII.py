e6ebd7036c3901e1ad08deeb44d665a0b7549e1a
pushpa.info.14 <pushpa.info.14@gmail.com>
2026-05-31 21:52:00 +0530

    leetcode 1871

--- LeetCode/1500-2000/Q1871 Jump Game VII.py
+++ LeetCode/1500-2000/Q1871 Jump Game VII.py
@@ -9,11 +9,31 @@ class Solution:
             if s[i] == '1':
                 continue
             j = bisect.bisect_right(prev, i - minJump)
-            if j and i <= prev[j - 1] + maxJump:
+            if j > 0 and i <= prev[j - 1] + maxJump:
                 prev.append(i)
         return prev[-1] == n - 1
 
+    def canReach2(self, s: str, minJump: int, maxJump: int) -> bool:
+        n = len(s)
+        if s[-1] == '1':
+            return False
+        diff = [0] * (n + maxJump + 1)
+        diff[minJump] += 1
+        diff[maxJump + 1] -= 1
+        for i in range(1, n):
+            diff[i] += diff[i - 1]
+            if s[i] == '1' or diff[i] == 0:
+                continue
+            diff[i + minJump] += 1
+            diff[i + maxJump + 1] -= 1
+        return diff[n - 1] > 0
+
 
 s = Solution()
+print(s.canReach(s="001010", minJump=2, maxJump=3))
 print(s.canReach(s="011010", minJump=2, maxJump=3))
 print(s.canReach(s="01101110", minJump=2, maxJump=3))
+print('-------------')
+print(s.canReach2(s="001010", minJump=2, maxJump=3))
+print(s.canReach2(s="011010", minJump=2, maxJump=3))
+print(s.canReach2(s="01101110", minJump=2, maxJump=3))
