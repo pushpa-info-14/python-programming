cb1f4153be758a86b952de3cd5ddf67f7f2dde66
pushpa.info.14 <pushpa.info.14@gmail.com>
2026-06-28 19:31:13 +0530

    leetcode 1846

---
+++ LeetCode/1500-2000/Q1846 Maximum Element After Decreasing and Rearranging.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        arr.sort()
+        arr[0] = 1
+        for i in range(1, len(arr)):
+            if arr[i] - arr[i - 1] > 1:
+                arr[i] = arr[i - 1] + 1
+        return arr[-1]
+
+
+s = Solution()
+print(s.maximumElementAfterDecrementingAndRearranging(arr=[2, 2, 1, 2, 1]))
+print(s.maximumElementAfterDecrementingAndRearranging(arr=[100, 1, 1000]))
+print(s.maximumElementAfterDecrementingAndRearranging(arr=[1, 2, 3, 4, 5]))
