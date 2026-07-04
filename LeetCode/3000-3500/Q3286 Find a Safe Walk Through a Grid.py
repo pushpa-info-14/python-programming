661a1dcf5753580b80939638e4ef4c793b589cdd
pushpa.info.14 <pushpa.info.14@gmail.com>
2026-07-04 19:48:58 +0530

    leetcode 3286

--- LeetCode/3000-3500/Q3286 Find a Safe Walk Through a Grid.py
+++ LeetCode/3000-3500/Q3286 Find a Safe Walk Through a Grid.py
@@ -1,4 +1,6 @@
 import heapq
+from collections import deque
+from math import inf
 from typing import List
 
 
@@ -31,9 +33,37 @@ class Solution:
                 visited.add((nr, nc))
         return False
 
+    def findSafeWalk2(self, grid: List[List[int]], health: int) -> bool:
+        m = len(grid)
+        n = len(grid[0])
+        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        dist = [[inf] * n for _ in range(m)]
+        dist[0][0] = grid[0][0]
+        q = deque()
+        q.append((grid[0][0], 0, 0))
+        while q:
+            d, r, c = q.popleft()
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if nr < 0 or nr == m or nc < 0 or nc == n:
+                    continue
+                nd = d + grid[nr][nc]
+                if nd < dist[nr][nc]:
+                    dist[nr][nc] = nd
+                    if d == nd:
+                        q.appendleft((nd, nr, nc))
+                    else:
+                        q.append((nd, nr, nc))
+        return dist[-1][-1] < health
+
 
 s = Solution()
 print(s.findSafeWalk(grid=[[0, 1, 0, 0, 0], [0, 1, 0, 1, 0], [0, 0, 0, 1, 0]], health=1))
 print(s.findSafeWalk(grid=[[0, 1, 1, 0, 0, 0], [1, 0, 1, 0, 0, 0], [0, 1, 1, 1, 0, 1], [0, 0, 1, 0, 1, 0]], health=3))
 print(s.findSafeWalk(grid=[[1, 1, 1], [1, 0, 1], [1, 1, 1]], health=5))
 print(s.findSafeWalk(grid=[[1, 1, 1, 1]], health=4))
+print("--------------")
+print(s.findSafeWalk2(grid=[[0, 1, 0, 0, 0], [0, 1, 0, 1, 0], [0, 0, 0, 1, 0]], health=1))
+print(s.findSafeWalk2(grid=[[0, 1, 1, 0, 0, 0], [1, 0, 1, 0, 0, 0], [0, 1, 1, 1, 0, 1], [0, 0, 1, 0, 1, 0]], health=3))
+print(s.findSafeWalk2(grid=[[1, 1, 1], [1, 0, 1], [1, 1, 1]], health=5))
+print(s.findSafeWalk2(grid=[[1, 1, 1, 1]], health=4))
