1d983a135a4fbf19711aaaf610a4d0dcf091b705
pushpa.info.14 <pushpa.info.14@gmail.com>
2026-07-04 19:57:00 +0530

    leetcode 2492

--- LeetCode/2000-2500/Q2492 Minimum Score of a Path Between Two Cities.py
+++ LeetCode/2000-2500/Q2492 Minimum Score of a Path Between Two Cities.py
@@ -5,25 +5,21 @@ from typing import List
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
         adj = defaultdict(list)
-        for u, v, distance in roads:
-            adj[u].append([v, distance])
-            adj[v].append([u, distance])
+        for u, v, w in roads:
+            adj[u].append([v, w])
+            adj[v].append([u, w])
 
         res = 10 ** 10
-        q = deque()
-        q.append((1, -1))  # node, previous
-        visited = set()
-        visited.add(1)
+        q = deque([1])
+        visited = {1}
         while q:
-            node, prev = q.popleft()
+            node = q.popleft()
             for nei, d in adj[node]:
-                if nei == prev:
-                    continue
                 res = min(res, d)
                 if nei in visited:
                     continue
                 visited.add(nei)
-                q.append((nei, node))
+                q.append(nei)
         return res
 
 
